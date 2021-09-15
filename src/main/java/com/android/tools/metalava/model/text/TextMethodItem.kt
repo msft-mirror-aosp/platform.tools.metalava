0e6d576088c650eedfe480a149da9f8e6a671d27
Anton Hansson <hansson@google.com>
2021-09-15 14:21:14 +0100

    Improve lookup of @return documentation
    Previously, when deciding whether to insert @return documentation for
    nullability, metalava inspected the whole method documentation,
    including parameter documentation. The result of this was that if a
    parameter doc mentioned nullability, we'd sometimes skip inserting
    @return nullability information.
    
    Improve this so that we only look at the subset of the documentation
    that would reasonably refer to the return value of the method.
    
    Bug: 186197911
    Test: tests in this cl
    Test: diff framework-doc-stubs before & after
    Merged-In: I15af628dc05d532aa67a12a374a4b1e0834b9167
    Change-Id: I15af628dc05d532aa67a12a374a4b1e0834b9167

--- src/main/java/com/android/tools/metalava/model/text/TextMethodItem.kt
+++ src/main/java/com/android/tools/metalava/model/text/TextMethodItem.kt
@@ -108,6 +108,8 @@ open class TextMethodItem(
         return list
     }
 
+    override fun findMainDocumentation(): String = documentation
+
     override fun findPredicateSuperMethod(predicate: Predicate<Item>): MethodItem? = null
 
     private var typeParameterList: TypeParameterList = TypeParameterList.NONE
