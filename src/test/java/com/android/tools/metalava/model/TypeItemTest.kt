61c2c3e5b99743debbda6b0e675b37aab5180297
Aurimas Liutikas <aurimas@google.com>
2022-02-24 09:52:12 -0800

    Remove support for android.support.annotation.
    We have migrated these to androidx.annotation 3+ years ago,
    it is time to remove the code for it in metalava.
    
    Test: ./gradlew test
    Change-Id: I418020371158357b01ccd05edeac2c4ac7e606e5

--- src/test/java/com/android/tools/metalava/model/TypeItemTest.kt
+++ src/test/java/com/android/tools/metalava/model/TypeItemTest.kt
@@ -23,11 +23,11 @@ import org.junit.Test
 class TypeItemTest {
     @Test
     fun test() {
-        assertThat(TypeItem.shortenTypes("@android.support.annotation.Nullable")).isEqualTo("@Nullable")
+        assertThat(TypeItem.shortenTypes("@androidx.annotation.Nullable")).isEqualTo("@Nullable")
         assertThat(TypeItem.shortenTypes(JAVA_LANG_STRING)).isEqualTo("String")
         assertThat(TypeItem.shortenTypes("java.lang.reflect.Method")).isEqualTo("java.lang.reflect.Method")
         assertThat(TypeItem.shortenTypes("java.util.List<java.lang.String>")).isEqualTo("java.util.List<java.lang.String>")
-        assertThat(TypeItem.shortenTypes("java.util.List<@android.support.annotation.NonNull java.lang.String>")).isEqualTo(
+        assertThat(TypeItem.shortenTypes("java.util.List<@androidx.annotation.NonNull java.lang.String>")).isEqualTo(
             "java.util.List<@NonNull java.lang.String>"
         )
     }
