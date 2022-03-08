4c8e76f12d32cf17f83c39a4b7f37de8c811cbff
Sam Gilbert <sjgilbert@google.com>
2022-03-08 14:59:32 -0500

    Make REMOVED_FINAL an ERROR
    Stop reporting it in places we don't care about
    
    Test: Further up CL stack
    Change-Id: I6e6bdec2575d775be3031499ee7fd664d647af46

--- src/main/java/com/android/tools/metalava/CompatibilityCheck.kt
+++ src/main/java/com/android/tools/metalava/CompatibilityCheck.kt
@@ -285,10 +285,6 @@ class CompatibilityCheck(
                         Issues.ADDED_FINAL, new, "${describe(new, capitalize = true)} added 'final' qualifier"
                     )
                 }
-            } else if (oldModifiers.isFinal() && !newModifiers.isFinal()) {
-                report(
-                    Issues.REMOVED_FINAL, new, "${describe(new, capitalize = true)} removed 'final' qualifier"
-                )
             }
 
             if (oldModifiers.isStatic() != newModifiers.isStatic()) {
@@ -497,10 +493,6 @@ class CompatibilityCheck(
                     report(
                         Issues.ADDED_FINAL, new, "${describe(new, capitalize = true)} has added 'final' qualifier"
                     )
-                } else if (old.isEffectivelyFinal() && !new.isEffectivelyFinal()) {
-                    report(
-                        Issues.REMOVED_FINAL, new, "${describe(new, capitalize = true)} has removed 'final' qualifier"
-                    )
                 }
             }
         }
@@ -614,14 +606,14 @@ class CompatibilityCheck(
                 val message = "${describe(new, capitalize = true)} has changed type from $oldType to $newType"
                 report(Issues.CHANGED_TYPE, new, message)
             } else if (!old.hasSameValue(new)) {
-                val prevValue = old.initialValue(true)
+                val prevValue = old.initialValue()
                 val prevString = if (prevValue == null && !old.modifiers.isFinal()) {
                     "nothing/not constant"
                 } else {
                     prevValue
                 }
 
-                val newValue = new.initialValue(true)
+                val newValue = new.initialValue()
                 val newString = if (newValue is PsiField) {
                     newValue.containingClass?.qualifiedName + "." + newValue.name
                 } else {
@@ -669,7 +661,11 @@ class CompatibilityCheck(
             report(
                 Issues.ADDED_FINAL, new, "${describe(new, capitalize = true)} has added 'final' qualifier"
             )
-        } else if (oldModifiers.isFinal() && !newModifiers.isFinal()) {
+        } else if (
+            // Final can't be removed if field is static with compile-time constant
+            oldModifiers.isFinal() && !newModifiers.isFinal() &&
+            oldModifiers.isStatic() && old.initialValue() != null
+        ) {
             report(
                 Issues.REMOVED_FINAL, new, "${describe(new, capitalize = true)} has removed 'final' qualifier"
             )
