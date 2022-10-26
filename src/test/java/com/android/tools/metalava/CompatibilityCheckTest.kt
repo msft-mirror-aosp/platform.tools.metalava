f6f476237f3af2b946953783dc320b45c10c04a2
Roberto Gil <robertogil@google.com>
2022-10-26 20:43:06 +0000

    Metalava fails to detect incompatibility in module build
    Incompatible changes to a released System API were not detected, this CL fixes the problem caused when a partial codebase is passed to MergedCodebase and merged with a full codebase removing the changes made in the partial codebase. When the list codebases (mergedcodebase) is passed to ComparisonVisitor.Compare() the first one is  used as master so the new codebase should be first or the changes are lost.
    
    Bug: 223771856
    Test: ./gradlew test
    Change-Id: Ic314b5c220074339f0f7f2df26cf337b2dc1d6de

--- src/test/java/com/android/tools/metalava/CompatibilityCheckTest.kt
+++ src/test/java/com/android/tools/metalava/CompatibilityCheckTest.kt
@@ -1777,6 +1777,94 @@ CompatibilityCheckTest : DriverTest() {
     }
 
     @Test
+    fun `Incompatible Changes in Released System API `() {
+        // Incompatible changes to a released System API should be detected
+        // In this case removing final and changing value of constant
+        check(
+            includeSystemApiAnnotations = true,
+            expectedIssues = """
+                src/android/rolecontrollerservice/RoleControllerService.java:8: error: Method android.rolecontrollerservice.RoleControllerService.sendNetworkScore has removed 'final' qualifier [RemovedFinal]
+                src/android/rolecontrollerservice/RoleControllerService.java:9: error: Field android.rolecontrollerservice.RoleControllerService.APP_RETURN_UNWANTED has changed value from 1 to 0 [ChangedValue]
+                """,
+            sourceFiles = arrayOf(
+                java(
+                    """
+                    package android.rolecontrollerservice;
+                    import android.annotation.SystemApi;
+
+                    /** @hide */
+                    @SystemApi
+                    public abstract class RoleControllerService {
+                        public abstract void onGrantDefaultRoles();
+                        public void sendNetworkScore();
+                        public static final int APP_RETURN_UNWANTED = 0;
+                    }
+                    """
+                ),
+                systemApiSource
+            ),
+
+            extraArguments = arrayOf(
+                ARG_SHOW_ANNOTATION, "android.annotation.TestApi",
+                ARG_HIDE_PACKAGE, "android.annotation",
+            ),
+
+            checkCompatibilityApiReleased =
+            """
+                package android.rolecontrollerservice {
+                  public abstract class RoleControllerService {
+                    ctor public RoleControllerService();
+                    method public abstract void onGrantDefaultRoles();
+                    method public final void sendNetworkScore();
+                    field public static final int APP_RETURN_UNWANTED = 1;
+                  }
+                }
+                """
+        )
+    }
+
+    @Test
+    fun `Incompatible changes to released API signature codebase`() {
+        // Incompatible changes to a released System API should be detected
+        // in case of partial files
+        check(
+            expectedIssues = """
+                TESTROOT/released-api.txt:5: error: Removed method test.pkg.Foo.method2() [RemovedMethod]
+                """,
+            signatureSource = """
+                // Signature format: 3.0
+                package test.pkg {
+                  public final class Foo {
+                    ctor public Foo();
+                    method public void method1();
+                  }
+                }
+                """,
+
+            checkCompatibilityApiReleased =
+            """
+                package test.pkg {
+                  public final class Foo {
+                    ctor public Foo();
+                    method public void method1();
+                    method public void method2();
+                    method public void method3();
+                  }
+                }
+                """,
+            checkCompatibilityBaseApi =
+            """
+                package test.pkg {
+                  public final class Foo {
+                    ctor public Foo();
+                    method public void method3();
+                  }
+                }
+                """,
+        )
+    }
+
+    @Test
     fun `Partial text file which adds methods to show-annotation API`() {
         // This happens in system and test files where we only include APIs that differ
         // from the base IDE. When parsing these code bases we need to gracefully handle
