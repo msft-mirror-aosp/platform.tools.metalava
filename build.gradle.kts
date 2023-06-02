793e3ad5ad16888d0668c5299e8d1fec512bcadd
Paul Duffin <paulduffin@google.com>
2023-06-02 10:37:06 +0100

    Resolve some issues with prebuilts/sdk emulation in build.gradle.kts
    Specifically, this change:
    * Dedups usages of `prebuilts/sdk`.
    * Avoids accessing internals of gradle `task.outputs`.
    * Uses File to avoid string manipulation.
    * Improves formatting.
    * Stops using `getBuildDirectory()` which is based on OUT_DIR for the
      fake prebuilts/sdk directory to avoid making the tests dependent on
      OUT_DIR (via the METALAVA_TEST_PREBUILTS_SDK_ROOT env variable). That
      would reduce the benefit of remote caching as OUT_DIR can change from
      one build to the next and is often set on build bots but not on
      developer machines.
    * Removes unnecessary `Assemble::class` from task registration.
    
    Bug: 285312164
    Test: ./gradlew clean test --no-build-cache
          ./gradlew
    Change-Id: Iddc2bd38538ecc8c366fe6fa267204aea50a9c50

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,11 @@ import java.io.FileInputStream
 import java.io.FileNotFoundException
 import java.util.Properties
 
+/**
+ * Take a copy of the original [buildDir] for use by [testPrebuiltsSdkDir]
+ */
+val originalBuildDir = buildDir
+
 buildDir = getBuildDirectory()
 
 defaultTasks = mutableListOf(
@@ -100,45 +105,84 @@ val zipTask: TaskProvider<Zip> = project.tasks.register(
     archiveFileName.set("metalava-tests.zip")
 }
 
-fun registerTestPrebuiltsSdkTasks(sourceDir: String, destJar: String): String {
+/**
+ * The location into which a fake representation of the prebuilts/sdk directory will be written.
+ *
+ * This uses [originalBuildDir] rather than [buildDir] as the latter depends on the `OUT_DIR`
+ * environment variable. As this value is passed in to the tests this would make the tests dependent
+ * on the `OUT_DIR` which as it varies from build to build would reduce the benefit of remote
+ * caching.
+ */
+val testPrebuiltsSdkDir = originalBuildDir.resolve("prebuilts/sdk")
+
+/**
+ * Register tasks to emulate parts of the prebuilts/sdk repository using source from this
+ * directory.
+ *
+ * [sourceDir] is the path to the root directory of code that is compiled into a jar that
+ * corresponds to the jar specified by [destJar].
+ *
+ * [destJar] is the path to a jar within prebuilts/sdk. The fake jar created from [sourceDir] is
+ * copied to getBuildDirectory()/prebuilts/sdk/$destJar.
+ *
+ * The jars created by this can be accessed by tests via the `METALAVA_TEST_PREBUILTS_SDK_ROOT`
+ * environment variable which is set to point to getBuildDirectory()/prebuilts/sdk; see [testTask].
+ */
+fun registerTestPrebuiltsSdkTasks(sourceDir: String, destJar: String): TaskProvider<Jar> {
     val basename = sourceDir.replace("/", "-")
     val javaCompileTaskName = "$basename.classes"
     val jarTaskName = "$basename.jar"
 
-    project.tasks.register(javaCompileTaskName, JavaCompile::class) {
+    val compileTask = project.tasks.register(javaCompileTaskName, JavaCompile::class) {
         options.compilerArgs = listOf("--patch-module", "java.base=" + file(sourceDir))
         source = fileTree(sourceDir)
         classpath = project.files()
-        destinationDirectory.set(File(getBuildDirectory(), javaCompileTaskName))
+        destinationDirectory.set(originalBuildDir.resolve(javaCompileTaskName))
     }
 
-    val dir = destJar.substringBeforeLast("/")
-    val filename = destJar.substringAfterLast("/")
-    if (dir == filename) {
+    val destJarFile = File(destJar)
+    val dir = destJarFile.parent
+    val filename = destJarFile.name
+    if (dir == ".") {
         throw IllegalArgumentException("bad destJar argument '$destJar'")
     }
 
-    project.tasks.register(jarTaskName, Jar::class) {
-        from(tasks.named(javaCompileTaskName).get().outputs.files.filter { it.isDirectory })
+    val jarTask = project.tasks.register(jarTaskName, Jar::class) {
+        from(compileTask.flatMap { it.destinationDirectory })
         archiveFileName.set(filename)
-        destinationDirectory.set(File(getBuildDirectory(), dir))
+        destinationDirectory.set(testPrebuiltsSdkDir.resolve(dir))
     }
 
-    return jarTaskName
+    return jarTask
 }
 
-val testPrebuiltsSdkApi30 = registerTestPrebuiltsSdkTasks("src/testdata/prebuilts-sdk-test/30", "prebuilts/sdk/30/public/android.jar")
-val testPrebuiltsSdkApi31 = registerTestPrebuiltsSdkTasks("src/testdata/prebuilts-sdk-test/31", "prebuilts/sdk/31/public/android.jar")
-val testPrebuiltsSdkExt1 = registerTestPrebuiltsSdkTasks("src/testdata/prebuilts-sdk-test/extensions/1", "prebuilts/sdk/extensions/1/public/framework-ext.jar")
-val testPrebuiltsSdkExt2 = registerTestPrebuiltsSdkTasks("src/testdata/prebuilts-sdk-test/extensions/2", "prebuilts/sdk/extensions/2/public/framework-ext.jar")
-val testPrebuiltsSdkExt3 = registerTestPrebuiltsSdkTasks("src/testdata/prebuilts-sdk-test/extensions/3", "prebuilts/sdk/extensions/3/public/framework-ext.jar")
+val testPrebuiltsSdkApi30 = registerTestPrebuiltsSdkTasks(
+    "src/testdata/prebuilts-sdk-test/30",
+    "30/public/android.jar"
+)
+val testPrebuiltsSdkApi31 = registerTestPrebuiltsSdkTasks(
+    "src/testdata/prebuilts-sdk-test/31",
+    "31/public/android.jar"
+)
+val testPrebuiltsSdkExt1 = registerTestPrebuiltsSdkTasks(
+    "src/testdata/prebuilts-sdk-test/extensions/1",
+    "extensions/1/public/framework-ext.jar"
+)
+val testPrebuiltsSdkExt2 = registerTestPrebuiltsSdkTasks(
+    "src/testdata/prebuilts-sdk-test/extensions/2",
+    "extensions/2/public/framework-ext.jar"
+)
+val testPrebuiltsSdkExt3 = registerTestPrebuiltsSdkTasks(
+    "src/testdata/prebuilts-sdk-test/extensions/3",
+    "extensions/3/public/framework-ext.jar"
+)
 
 project.tasks.register("test-sdk-extensions-info.xml", Copy::class) {
     from("src/testdata/prebuilts-sdk-test/sdk-extensions-info.xml")
-    into(File(getBuildDirectory(), "prebuilts/sdk"))
+    into(testPrebuiltsSdkDir)
 }
 
-project.tasks.register("test-prebuilts-sdk", Assemble::class) {
+project.tasks.register("test-prebuilts-sdk") {
     dependsOn(testPrebuiltsSdkApi30)
     dependsOn(testPrebuiltsSdkApi31)
     dependsOn(testPrebuiltsSdkExt1)
@@ -150,7 +194,7 @@ project.tasks.register("test-prebuilts-sdk", Assemble::class) {
 val testTask = tasks.named("test", Test::class.java)
 testTask.configure {
     dependsOn("test-prebuilts-sdk")
-    setEnvironment("METALAVA_TEST_PREBUILTS_SDK_ROOT" to getBuildDirectory().path + "/prebuilts/sdk")
+    setEnvironment("METALAVA_TEST_PREBUILTS_SDK_ROOT" to testPrebuiltsSdkDir)
     jvmArgs = listOf("--add-opens=java.base/java.lang=ALL-UNNAMED")
     maxParallelForks = (Runtime.getRuntime().availableProcessors() / 2).takeIf { it > 0 } ?: 1
     testLogging.events = hashSetOf(
