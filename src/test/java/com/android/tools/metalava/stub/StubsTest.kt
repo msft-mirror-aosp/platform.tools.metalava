a07e78ca69f93c054acad47a90144e0fbb7db316
Paul Duffin <paulduffin@google.com>
2023-06-15 18:28:41 +0100

    Extract enum related stubs tests
    Reduces `./gradlew` time by about 1 second.
    
    Checked tests report:
    * 869 tests
    * 0 failures
    * 12 ignored
    
    Bug: 287473897
    Test: time ./gradlew --rerun-tasks
    Change-Id: I65e29ea4ee83a8440c559f41af419d2e84fe7e91

--- src/test/java/com/android/tools/metalava/stub/StubsTest.kt
+++ src/test/java/com/android/tools/metalava/stub/StubsTest.kt
@@ -199,35 +199,6 @@ class StubsTest : AbstractStubsTest() {
     }
 
     @Test
-    fun `Generate stubs for enum`() {
-        // Interface: makes sure the right modifiers etc are shown (and that "package private" methods
-        // in the interface are taken to be public etc)
-        checkStubs(
-            sourceFiles = arrayOf(
-                java(
-                    """
-                    package test.pkg;
-                    @SuppressWarnings("ALL")
-                    public enum Foo {
-                        A, /** @deprecated */ @Deprecated B;
-                    }
-                    """
-                )
-            ),
-            source = """
-                package test.pkg;
-                @SuppressWarnings({"unchecked", "deprecation", "all"})
-                public enum Foo {
-                A,
-                /** @deprecated */
-                @Deprecated
-                B;
-                }
-                """
-        )
-    }
-
-    @Test
     fun `Generate stubs for class with superclass`() {
         // Make sure superclass statement is correct; unlike signature files, inherited method from parent
         // that has same signature should be included in the child
@@ -418,95 +389,6 @@ class StubsTest : AbstractStubsTest() {
     }
 
     @Test
-    fun `Generate stubs for class with abstract enum methods`() {
-        // As per https://bugs.openjdk.java.net/browse/JDK-6287639
-        // abstract methods in enums should not be listed as abstract,
-        // but doclava1 does, so replicate this.
-        // Also checks that we handle both enum fields and regular fields
-        // and that they are listed separately.
-
-        checkStubs(
-            sourceFiles = arrayOf(
-                java(
-                    """
-                    package test.pkg;
-
-                    public enum FooBar {
-                        /** My 1st documentation */
-                        ABC {
-                            @Override
-                            protected void foo() { }
-                        },
-                        /** My 2nd documentation */
-                        DEF {
-                            @Override
-                            protected void foo() { }
-                        };
-
-                        protected abstract void foo();
-                        public static int field1 = 1;
-                        public int field2 = 2;
-                    }
-                    """
-                )
-            ),
-            source = """
-                package test.pkg;
-                @SuppressWarnings({"unchecked", "deprecation", "all"})
-                public enum FooBar {
-                /** My 1st documentation */
-                ABC,
-                /** My 2nd documentation */
-                DEF;
-                protected void foo() { throw new RuntimeException("Stub!"); }
-                public static int field1 = 1; // 0x1
-                public int field2 = 2; // 0x2
-                }
-                """
-        )
-    }
-
-    @Test
-    fun `Skip hidden enum constants in stubs`() {
-        checkStubs(
-            sourceFiles = arrayOf(
-                java(
-                    """
-                    package test.pkg;
-                    public enum Alignment {
-                        ALIGN_NORMAL,
-                        ALIGN_OPPOSITE,
-                        ALIGN_CENTER,
-                        /** @hide */
-                        ALIGN_LEFT,
-                        /** @hide */
-                        ALIGN_RIGHT
-                    }
-                    """
-                )
-            ),
-            api = """
-                package test.pkg {
-                  public enum Alignment {
-                    enum_constant public static final test.pkg.Alignment ALIGN_CENTER;
-                    enum_constant public static final test.pkg.Alignment ALIGN_NORMAL;
-                    enum_constant public static final test.pkg.Alignment ALIGN_OPPOSITE;
-                  }
-                }
-            """,
-            source = """
-                package test.pkg;
-                @SuppressWarnings({"unchecked", "deprecation", "all"})
-                public enum Alignment {
-                ALIGN_NORMAL,
-                ALIGN_OPPOSITE,
-                ALIGN_CENTER;
-                }
-            """
-        )
-    }
-
-    @Test
     fun `Check correct throws list for generics`() {
         checkStubs(
             sourceFiles = arrayOf(
@@ -602,61 +484,6 @@ class StubsTest : AbstractStubsTest() {
                 }
                 """,
             checkTextStubEquivalence = true
-        )
-    }
-
-    @Test
-    fun `Generate stubs enum instance methods`() {
-        checkStubs(
-            sourceFiles = arrayOf(
-                java(
-                    """
-                    package test.pkg;
-
-                    public enum ChronUnit implements TempUnit {
-                        C(1), B(2), A(3);
-
-                        ChronUnit(int y) {
-                        }
-
-                        public String valueOf(int x) {
-                            return Integer.toString(x + 5);
-                        }
-
-                        public String values(String separator) {
-                            return null;
-                        }
-
-                        @Override
-                        public String toString() {
-                            return name();
-                        }
-                    }
-                    """
-                ),
-                java(
-                    """
-                    package test.pkg;
-
-                    public interface TempUnit {
-                        @Override
-                        String toString();
-                    }
-                     """
-                )
-            ),
-            source = """
-                package test.pkg;
-                @SuppressWarnings({"unchecked", "deprecation", "all"})
-                public enum ChronUnit implements test.pkg.TempUnit {
-                C,
-                B,
-                A;
-                public java.lang.String valueOf(int x) { throw new RuntimeException("Stub!"); }
-                public java.lang.String values(java.lang.String separator) { throw new RuntimeException("Stub!"); }
-                public java.lang.String toString() { throw new RuntimeException("Stub!"); }
-                }
-            """
         )
     }
 
