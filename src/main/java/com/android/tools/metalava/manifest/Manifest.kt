a8eceb2a4454740b3db3221f4870c2671ab1786c
Paul Duffin <paulduffin@google.com>
2023-06-30 16:45:07 +0100

    Move Xml.kt out of the model package
    Since the `Manifest` has been extracted this is no longer used in the
    model.
    
    Bug: 287241408
    Test: ./gradlew
    Change-Id: I6bf64a4313d69e191781da52f0be2682660d31ef

--- src/main/java/com/android/tools/metalava/manifest/Manifest.kt
+++ src/main/java/com/android/tools/metalava/manifest/Manifest.kt
@@ -21,8 +21,8 @@ import com.android.tools.metalava.Issues
 import com.android.tools.metalava.model.MinSdkVersion
 import com.android.tools.metalava.model.SetMinSdkVersion
 import com.android.tools.metalava.model.UnsetMinSdkVersion
-import com.android.tools.metalava.model.parseDocument
 import com.android.tools.metalava.reporter
+import com.android.tools.metalava.xml.parseDocument
 import com.android.utils.XmlUtils
 import java.io.File
 
