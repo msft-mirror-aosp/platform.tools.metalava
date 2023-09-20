75b3927e72f1b6aa87417a43206dbb0a0e3beb33
Pratyush <pratyushpks@google.com>
2023-09-20 10:46:41 +0000

    Add test for class hierarchy
    Bug: 299048934
    Test: ./gradlew
    Change-Id: Id9b8c1c9774be581291d3de66e063de3542a4851

--- metalava-model-testsuite/src/main/java/com/android/tools/metalava/model/testsuite/BootstrapSourceModelProviderTest.kt
+++ metalava-model-testsuite/src/main/java/com/android/tools/metalava/model/testsuite/BootstrapSourceModelProviderTest.kt
@@ -156,4 +156,31 @@ class BootstrapSourceModelProviderTest(parameters: TestParameters) : BaseModelTe
             assertEquals(classItem, innerClassItem.containingClass())
         }
     }
+
+    @Test
+    fun `090 - check class hierarchy`() {
+        runSourceCodebaseTest(
+            java(
+                """
+                    package test.pkg;
+
+                    interface SuperInterface{}
+                    abstract class SuperClass implements SuperInterface{}
+                    interface ChildInterface {}
+
+                    class Test extends SuperClass implements ChildInterface {
+                    }
+                """
+            ),
+        ) { codebase ->
+            val classItem = codebase.assertClass("test.pkg.Test")
+            val superClassItem = codebase.assertClass("test.pkg.SuperClass")
+            val superInterfaceItem = codebase.assertClass("test.pkg.SuperInterface")
+            val childInterfaceItem = codebase.assertClass("test.pkg.ChildInterface")
+            assertEquals(superClassItem, classItem.superClass())
+            assertEquals(2, classItem.allInterfaces().count(), message = "")
+            assertEquals(true, classItem.allInterfaces().contains(childInterfaceItem))
+            assertEquals(true, classItem.allInterfaces().contains(superInterfaceItem))
+        }
+    }
 }
