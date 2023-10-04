a9914aad24bf403cd9f69d537852abcb668cb1ed
Julia McClellan <juliamcclellan@google.com>
2023-10-04 09:42:51 -0400

    Add tests for type-use annotations
    Test: added tests, currently baselined for all models
    Bug: 300081840
    Change-Id: I755f9e1f877c60ecc94bcc7ebed6c7ba5699ad1a

--- metalava-model-testsuite/src/main/java/com/android/tools/metalava/model/testsuite/typeitem/CommonTypeModifiersTest.kt
+++ metalava-model-testsuite/src/main/java/com/android/tools/metalava/model/testsuite/typeitem/CommonTypeModifiersTest.kt
@@ -18,6 +18,9 @@ package com.android.tools.metalava.model.testsuite.typeitem
 
 import com.android.tools.metalava.model.ArrayTypeItem
 import com.android.tools.metalava.model.ClassTypeItem
+import com.android.tools.metalava.model.Item
+import com.android.tools.metalava.model.PrimitiveTypeItem
+import com.android.tools.metalava.model.TypeItem
 import com.android.tools.metalava.model.VariableTypeItem
 import com.android.tools.metalava.model.testsuite.BaseModelTest
 import com.android.tools.metalava.model.testsuite.TestParameters
@@ -29,6 +32,279 @@ import org.junit.runners.Parameterized
 
 @RunWith(Parameterized::class)
 class CommonTypeModifiersTest(parameters: TestParameters) : BaseModelTest(parameters) {
+
+    private fun TypeItem.annotationNames(): List<String?> {
+        return modifiers.annotations().map { it.qualifiedName }
+    }
+
+    private fun Item.annotationNames(): List<String?> {
+        return modifiers.annotations().map { it.qualifiedName }
+    }
+
+    @Test
+    fun `Test annotation on basic types`() {
+        runCodebaseTest(
+            java(
+                """
+                    package test.pkg;
+                    import java.lang.annotation.ElementType;
+                    import java.lang.annotation.Target;
+                    public class Foo {
+                        public @test.pkg.A int foo1() {}
+                        public @test.pkg.A String foo2() {}
+                        public <T> @test.pkg.A T foo3() {}
+                    }
+                """
+            ),
+            signature(
+                """
+                    // Signature format: 2.0
+                    package test.pkg {
+                      public class Foo {
+                        method public @test.pkg.A int foo1();
+                        method public @test.pkg.A String foo2();
+                        method public <T> @test.pkg.A T foo3();
+                      }
+                    }
+                """
+                    .trimIndent()
+            )
+        ) { codebase ->
+            val methods = codebase.assertClass("test.pkg.Foo").methods()
+            assertThat(methods).hasSize(3)
+
+            // @test.pkg.A int
+            val primitiveMethod = methods[0]
+            val primitive = primitiveMethod.returnType()
+            assertThat(primitive).isInstanceOf(PrimitiveTypeItem::class.java)
+            // The annotation can be interpreted as applying to the method or return type
+            assertThat(primitive.annotationNames() + primitiveMethod.annotationNames())
+                .containsExactly("test.pkg.A")
+
+            // @test.pkg.A String
+            val stringMethod = methods[1]
+            val string = stringMethod.returnType()
+            assertThat(string).isInstanceOf(ClassTypeItem::class.java)
+            // The annotation can be interpreted as applying to the method or return type
+            assertThat(string.annotationNames() + stringMethod.annotationNames())
+                .containsExactly("test.pkg.A")
+
+            // test.pkg.@test.pkg.A Foo
+            val variableMethod = methods[2]
+            val variable = variableMethod.returnType()
+            assertThat(variable).isInstanceOf(VariableTypeItem::class.java)
+            assertThat(variable.annotationNames()).containsExactly("test.pkg.A")
+            // The annotation can be interpreted as applying to the method or return type
+            assertThat(variable.annotationNames() + variableMethod.annotationNames())
+                .containsExactly("test.pkg.A")
+        }
+    }
+
+    @Test
+    fun `Test annotations on qualified class type`() {
+        runCodebaseTest(
+            java(
+                """
+                    package test.pkg;
+                    public class Foo {
+                        public test.pkg.@test.pkg.A Foo foo() {}
+                    }
+                """
+            ),
+            signature(
+                """
+                    // Signature format: 2.0
+                    package test.pkg {
+                      public class Foo {
+                        method public test.pkg.@test.pkg.A Foo foo();
+                      }
+                    }
+                """
+                    .trimIndent()
+            )
+        ) { codebase ->
+            val method = codebase.assertClass("test.pkg.Foo").methods().single()
+            assertThat(method.annotationNames()).isEmpty()
+
+            val returnType = method.returnType()
+            assertThat(returnType).isInstanceOf(ClassTypeItem::class.java)
+            assertThat((returnType as ClassTypeItem).qualifiedName).isEqualTo("test.pkg.Foo")
+            assertThat(returnType.annotationNames()).containsExactly("test.pkg.A")
+        }
+    }
+
+    @Test
+    fun `Test annotations on class type parameters`() {
+        runCodebaseTest(
+            java(
+                """
+                    package test.pkg;
+
+                    public class Foo {
+                        public java.util.@test.pkg.A Map<java.lang.@test.pkg.B @test.pkg.C String, java.lang.@test.pkg.D String> foo() {}
+                    }
+                """
+            ),
+            signature(
+                """
+                    // Signature format: 2.0
+                    package test.pkg {
+                      public class Foo {
+                        method public java.util.@test.pkg.A Map<java.lang.@test.pkg.B @test.pkg.C String, java.lang.@test.pkg.D String> foo();
+                      }
+                    }
+                """
+                    .trimIndent()
+            )
+        ) { codebase ->
+            val method = codebase.assertClass("test.pkg.Foo").methods().single()
+            assertThat(method.annotationNames()).isEmpty()
+
+            val mapType = method.returnType()
+            assertThat(mapType).isInstanceOf(ClassTypeItem::class.java)
+            assertThat(mapType.annotationNames()).containsExactly("test.pkg.A")
+            assertThat((mapType as ClassTypeItem).parameters).hasSize(2)
+
+            // java.lang.@test.pkg.B @test.pkg.C String
+            val string1 = mapType.parameters[0]
+            assertThat(string1.isString()).isTrue()
+            assertThat(string1.annotationNames()).containsExactly("test.pkg.B", "test.pkg.C")
+
+            // java.lang.@test.pkg.D String
+            val string2 = mapType.parameters[1]
+            assertThat(string2.isString()).isTrue()
+            assertThat(string2.annotationNames()).containsExactly("test.pkg.D")
+        }
+    }
+
+    @Test
+    fun `Test annotations on array type and component type`() {
+        runCodebaseTest(
+            java(
+                """
+                    package test.pkg;
+                    public class Foo {
+                        public test.pkg.@test.pkg.A @test.pkg.B Foo @test.pkg.B @test.pkg.C [] foo() {}
+                    }
+                """
+            ),
+            signature(
+                """
+                    // Signature format: 2.0
+                    package test.pkg {
+                      public class Foo {
+                        method public test.pkg.@test.pkg.A @test.pkg.B Foo @test.pkg.B @test.pkg.C [] foo();
+                      }
+                    }
+                """
+                    .trimIndent()
+            )
+        ) { codebase ->
+            val method = codebase.assertClass("test.pkg.Foo").methods().single()
+            assertThat(method.annotationNames()).isEmpty()
+
+            val arrayType = method.returnType()
+            assertThat(arrayType).isInstanceOf(ArrayTypeItem::class.java)
+            assertThat(arrayType.annotationNames()).containsExactly("test.pkg.B", "test.pkg.C")
+
+            val componentType = (arrayType as ArrayTypeItem).componentType
+            assertThat(componentType).isInstanceOf(ClassTypeItem::class.java)
+            assertThat((componentType as ClassTypeItem).qualifiedName).isEqualTo("test.pkg.Foo")
+            assertThat(componentType.annotationNames()).containsExactly("test.pkg.A", "test.pkg.B")
+        }
+    }
+
+    @Test
+    fun `Test annotations on multidimensional array`() {
+        runCodebaseTest(
+            java(
+                """
+                    package test.pkg;
+                    public class Foo {
+                        public test.pkg.@test.pkg.A Foo @test.pkg.B [] @test.pkg.C [] @test.pkg.D [] foo() {}
+                    }
+                """
+                    .trimIndent()
+            ),
+            signature(
+                """
+                    // Signature format: 2.0
+                    package test.pkg {
+                      public class Foo {
+                        method public test.pkg.@test.pkg.A Foo @test.pkg.B [] @test.pkg.C [] @test.pkg.D [] foo();
+                      }
+                    }
+                """
+                    .trimIndent()
+            )
+        ) { codebase ->
+            val method = codebase.assertClass("test.pkg.Foo").methods().single()
+            assertThat(method.annotationNames()).isEmpty()
+
+            val outerArray = method.returnType()
+            assertThat(outerArray).isInstanceOf(ArrayTypeItem::class.java)
+            assertThat(outerArray.annotationNames()).containsExactly("test.pkg.B")
+
+            val middleArray = (outerArray as ArrayTypeItem).componentType
+            assertThat(middleArray).isInstanceOf(ArrayTypeItem::class.java)
+            assertThat(middleArray.annotationNames()).containsExactly("test.pkg.C")
+
+            val innerArray = (middleArray as ArrayTypeItem).componentType
+            assertThat(innerArray).isInstanceOf(ArrayTypeItem::class.java)
+            assertThat(innerArray.annotationNames()).containsExactly("test.pkg.D")
+
+            val componentType = (innerArray as ArrayTypeItem).componentType
+            assertThat(componentType).isInstanceOf(ClassTypeItem::class.java)
+            assertThat((componentType as ClassTypeItem).qualifiedName).isEqualTo("test.pkg.Foo")
+            assertThat(componentType.annotationNames()).containsExactly("test.pkg.A")
+        }
+    }
+
+    @Test
+    fun `Test annotations on multidimensional vararg array`() {
+        runCodebaseTest(
+            java(
+                """
+                    package test.pkg;
+                    public class Foo {
+                        public void foo(test.pkg.@test.pkg.A Foo @test.pkg.B [] @test.pkg.C [] @test.pkg.D ... arg) {}
+                    }
+                """
+                    .trimIndent()
+            ),
+            signature(
+                """
+                    // Signature format: 4.0
+                    package test.pkg {
+                      public class Foo {
+                        method public void foo(test.pkg.@test.pkg.A Foo @test.pkg.B [] @test.pkg.C [] @test.pkg.D ...);
+                      }
+                    }
+                """
+                    .trimIndent()
+            )
+        ) { codebase ->
+            val outerArray =
+                codebase.assertClass("test.pkg.Foo").methods().single().parameters().single().type()
+            assertThat(outerArray).isInstanceOf(ArrayTypeItem::class.java)
+            assertThat((outerArray as ArrayTypeItem).isVarargs).isTrue()
+            assertThat(outerArray.annotationNames()).containsExactly("test.pkg.B")
+
+            val middleArray = outerArray.componentType
+            assertThat(middleArray).isInstanceOf(ArrayTypeItem::class.java)
+            assertThat(middleArray.annotationNames()).containsExactly("test.pkg.C")
+
+            val innerArray = (middleArray as ArrayTypeItem).componentType
+            assertThat(innerArray).isInstanceOf(ArrayTypeItem::class.java)
+            assertThat(innerArray.annotationNames()).containsExactly("test.pkg.D")
+
+            val componentType = (innerArray as ArrayTypeItem).componentType
+            assertThat(componentType).isInstanceOf(ClassTypeItem::class.java)
+            assertThat((componentType as ClassTypeItem).qualifiedName).isEqualTo("test.pkg.Foo")
+            assertThat(componentType.annotationNames()).containsExactly("test.pkg.A")
+        }
+    }
+
     @Test
     fun `Test inner parameterized types with annotations`() {
         runCodebaseTest(
@@ -39,7 +315,7 @@ class CommonTypeModifiersTest(parameters: TestParameters) : BaseModelTest(parame
                         public class Inner<I> {
                         }
 
-                        public <P1, P2> @test.pkg.A Outer<@test.pkg.B P1>.@test.pkg.C Inner<@test.pkg.D P2> foo() {
+                        public <P1, P2> test.pkg.@test.pkg.A Outer<@test.pkg.B P1>.@test.pkg.C Inner<@test.pkg.D P2> foo() {
                             return new Outer<P1>.Inner<P2>();
                         }
                     }
@@ -67,26 +343,31 @@ class CommonTypeModifiersTest(parameters: TestParameters) : BaseModelTest(parame
             val p1 = methodTypeParameters[0]
             val p2 = methodTypeParameters[1]
 
-            // TODO: test the annotations
             // Outer<P1>.Inner<P2>
             val innerType = method.returnType()
             assertThat(innerType).isInstanceOf(ClassTypeItem::class.java)
             assertThat((innerType as ClassTypeItem).qualifiedName).isEqualTo("test.pkg.Outer.Inner")
             assertThat(innerType.parameters).hasSize(1)
+            assertThat(innerType.annotationNames()).containsExactly("test.pkg.C")
+
             val innerTypeParameter = innerType.parameters.single()
             assertThat(innerTypeParameter).isInstanceOf(VariableTypeItem::class.java)
             assertThat((innerTypeParameter as VariableTypeItem).name).isEqualTo("P2")
             assertThat(innerTypeParameter.asTypeParameter).isEqualTo(p2)
+            assertThat(innerTypeParameter.annotationNames()).containsExactly("test.pkg.D")
 
             val outerType = innerType.outerClassType
             assertThat(outerType).isNotNull()
             assertThat(outerType!!.qualifiedName).isEqualTo("test.pkg.Outer")
             assertThat(outerType.outerClassType).isNull()
             assertThat(outerType.parameters).hasSize(1)
+            assertThat(outerType.annotationNames()).containsExactly("test.pkg.A")
+
             val outerClassParameter = outerType.parameters.single()
             assertThat(outerClassParameter).isInstanceOf(VariableTypeItem::class.java)
             assertThat((outerClassParameter as VariableTypeItem).name).isEqualTo("P1")
             assertThat(outerClassParameter.asTypeParameter).isEqualTo(p1)
+            assertThat(outerClassParameter.annotationNames()).containsExactly("test.pkg.B")
         }
     }
 
@@ -117,6 +398,9 @@ class CommonTypeModifiersTest(parameters: TestParameters) : BaseModelTest(parame
             val bar = interfaces[0]
             assertThat(bar).isInstanceOf(ClassTypeItem::class.java)
             assertThat((bar as ClassTypeItem).qualifiedName).isEqualTo("test.pkg.Bar")
+            val annotations = bar.modifiers.annotations()
+            assertThat(annotations).hasSize(1)
+            assertThat(annotations.single().qualifiedName).isEqualTo("test.pkg.A")
 
             val baz = interfaces[1]
             assertThat(baz).isInstanceOf(ClassTypeItem::class.java)
@@ -149,6 +433,7 @@ class CommonTypeModifiersTest(parameters: TestParameters) : BaseModelTest(parame
             assertThat(superClass).isNotNull()
             assertThat(superClass).isInstanceOf(ClassTypeItem::class.java)
             assertThat((superClass as ClassTypeItem).qualifiedName).isEqualTo("test.pkg.Bar")
+            assertThat(superClass.annotationNames()).containsExactly("test.pkg.A")
         }
     }
 
@@ -158,14 +443,14 @@ class CommonTypeModifiersTest(parameters: TestParameters) : BaseModelTest(parame
             java(
                 """
                     package test.pkg;
-                    public interface Foo extends test.pkg.@test.pkg.A Bar, test.pkg.@test.pkg.A Baz<@test.pkg.A String>, test.pkg.Biz {}
+                    public interface Foo extends test.pkg.@test.pkg.A Bar, test.pkg.@test.pkg.B Baz<@test.pkg.C String>, test.pkg.Biz {}
                 """
             ),
             signature(
                 """
                     // Signature format: 4.0
                     package test.pkg {
-                      public interface Foo extends test.pkg.@test.pkg.A Bar test.pkg.@test.pkg.A Baz<@test.pkg.A String> test.pkg.Biz {
+                      public interface Foo extends test.pkg.@test.pkg.A Bar test.pkg.@test.pkg.B Baz<@test.pkg.C String> test.pkg.Biz {
                       }
                     }
                 """
@@ -178,6 +463,7 @@ class CommonTypeModifiersTest(parameters: TestParameters) : BaseModelTest(parame
             assertThat(bar).isNotNull()
             assertThat(bar).isInstanceOf(ClassTypeItem::class.java)
             assertThat((bar as ClassTypeItem).qualifiedName).isEqualTo("test.pkg.Bar")
+            assertThat(bar.annotationNames()).containsExactly("test.pkg.A")
 
             val interfaces = foo.interfaceTypes()
             assertThat(interfaces).hasSize(2)
@@ -186,12 +472,16 @@ class CommonTypeModifiersTest(parameters: TestParameters) : BaseModelTest(parame
             assertThat(baz).isInstanceOf(ClassTypeItem::class.java)
             assertThat((baz as ClassTypeItem).qualifiedName).isEqualTo("test.pkg.Baz")
             assertThat(baz.parameters).hasSize(1)
+            assertThat(baz.annotationNames()).containsExactly("test.pkg.B")
+
             val bazParam = baz.parameters.single()
             assertThat(bazParam.isString()).isTrue()
+            assertThat(bazParam.annotationNames()).containsExactly("test.pkg.C")
 
             val biz = interfaces[1]
             assertThat(biz).isInstanceOf(ClassTypeItem::class.java)
             assertThat((biz as ClassTypeItem).qualifiedName).isEqualTo("test.pkg.Biz")
+            assertThat(biz.annotationNames()).isEmpty()
         }
     }
 
@@ -231,10 +521,11 @@ class CommonTypeModifiersTest(parameters: TestParameters) : BaseModelTest(parame
 
             // Do full check for one type, then verify the others are equal
             assertThat(returnType).isInstanceOf(ArrayTypeItem::class.java)
+            assertThat(returnType.annotationNames()).containsExactly("test.pkg.A")
             val componentType = (returnType as ArrayTypeItem).componentType
             assertThat(componentType).isInstanceOf(ClassTypeItem::class.java)
             assertThat((componentType as ClassTypeItem).qualifiedName).isEqualTo("test.pkg.Foo")
-            // TODO: test the annotations
+            assertThat(componentType.annotationNames()).isEmpty()
 
             assertThat(returnType).isEqualTo(paramType)
             assertThat(returnType).isEqualTo(fieldType)
