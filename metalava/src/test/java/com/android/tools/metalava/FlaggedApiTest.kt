9fc2d585f39c569f3e721294aa9fa8d008ccc3d3
Paul Duffin <paulduffin@google.com>
2023-10-18 17:03:02 +0100

    Generalize the flagged test running
    Extracts code for testing flagged APIs in public and system and with
    and without the APIs annotated with `@FlaggedApi`.
    
    Bug: 306242007
    Test: ./gradlew
    Change-Id: Ifb3fa857d8ac5a7d0c04209982c171145ac6f4f1

--- metalava/src/test/java/com/android/tools/metalava/FlaggedApiTest.kt
+++ metalava/src/test/java/com/android/tools/metalava/FlaggedApiTest.kt
@@ -16,92 +16,57 @@
 
 package com.android.tools.metalava
 
+import com.android.tools.lint.checks.infrastructure.TestFile
 import com.android.tools.metalava.model.text.FileFormat
 import com.android.tools.metalava.testing.java
 import org.junit.Test
 
+private val annotationsList = listOf(systemApiSource, flaggedApiSource)
+
 class FlaggedApiTest : DriverTest() {
-    @Test
-    fun `FlaggedApi annotated items can be hidden if requested via command line`() {
+    /**
+     * Check the result of generating APIs with and without flagged apis for both public and system
+     * API surfaces.
+     */
+    private fun checkFlaggedApis(
+        vararg sourceFiles: TestFile,
+        expectedPublicApi: String,
+        expectedPublicApiMinusFlaggedApi: String,
+        expectedSystemApi: String,
+        expectedSystemApiMinusFlaggedApi: String,
+    ) {
         fun checkFlaggedApi(api: String, extraArguments: Array<String>) {
             check(
                 format = FileFormat.V2,
                 sourceFiles =
-                    arrayOf(
-                        java(
-                            """
-                        package test.pkg;
-
-                        import android.annotation.FlaggedApi;
-                        import android.annotation.SystemApi;
-
-                        public class Foo {
-                            @FlaggedApi("foo/bar")
-                            public void flaggedPublicApi() {}
-
-                            /** @hide */
-                            @SystemApi
-                            @FlaggedApi("foo/bar")
-                            public void flaggedSystemApi() {}
+                    buildList {
+                            addAll(sourceFiles)
+                            addAll(annotationsList)
                         }
-                            """
-                        ),
-                        systemApiSource,
-                        flaggedApiSource
-                    ),
+                        .toTypedArray(),
                 api = api,
                 extraArguments = arrayOf(ARG_HIDE_PACKAGE, "android.annotation") + extraArguments
             )
         }
 
         // public api scope, including flagged APIs
-        checkFlaggedApi(
-            api =
-                """
-                // Signature format: 2.0
-                package test.pkg {
-                  public class Foo {
-                    ctor public Foo();
-                    method @FlaggedApi("foo/bar") public void flaggedPublicApi();
-                  }
-                }
-                """,
-            extraArguments = arrayOf()
-        )
+        checkFlaggedApi(api = expectedPublicApi, extraArguments = arrayOf())
 
         // public api scope, excluding flagged APIs
         checkFlaggedApi(
-            api =
-                """
-                // Signature format: 2.0
-                package test.pkg {
-                  public class Foo {
-                    ctor public Foo();
-                  }
-                }
-                """,
+            api = expectedPublicApiMinusFlaggedApi,
             extraArguments = arrayOf(ARG_HIDE_ANNOTATION, "android.annotation.FlaggedApi")
         )
 
         // system api scope, including flagged APIs
         checkFlaggedApi(
-            api =
-                """
-                // Signature format: 2.0
-                package test.pkg {
-                  public class Foo {
-                    method @FlaggedApi("foo/bar") public void flaggedSystemApi();
-                  }
-                }
-                """,
+            api = expectedSystemApi,
             extraArguments = arrayOf(ARG_SHOW_ANNOTATION, "android.annotation.SystemApi")
         )
 
         // system api scope, excluding flagged APIs
         checkFlaggedApi(
-            api = """
-                // Signature format: 2.0
-                """,
+            api = expectedSystemApiMinusFlaggedApi,
             extraArguments =
                 arrayOf(
                     ARG_SHOW_ANNOTATION,
@@ -109,6 +74,62 @@ class FlaggedApiTest : DriverTest() {
                     ARG_HIDE_ANNOTATION,
                     "android.annotation.FlaggedApi"
                 )
+        )
+    }
+
+    @Test
+    fun `Basic test that FlaggedApi annotated items can be hidden`() {
+        checkFlaggedApis(
+            java(
+                """
+                    package test.pkg;
+
+                    import android.annotation.FlaggedApi;
+                    import android.annotation.SystemApi;
+
+                    public class Foo {
+                        @FlaggedApi("foo/bar")
+                        public void flaggedPublicApi() {}
+
+                        /** @hide */
+                        @SystemApi
+                        @FlaggedApi("foo/bar")
+                        public void flaggedSystemApi() {}
+                    }
+                """
+            ),
+            expectedPublicApi =
+                """
+                    // Signature format: 2.0
+                    package test.pkg {
+                      public class Foo {
+                        ctor public Foo();
+                        method @FlaggedApi("foo/bar") public void flaggedPublicApi();
+                      }
+                    }
+                """,
+            expectedPublicApiMinusFlaggedApi =
+                """
+                    // Signature format: 2.0
+                    package test.pkg {
+                      public class Foo {
+                        ctor public Foo();
+                      }
+                    }
+                """,
+            expectedSystemApi =
+                """
+                    // Signature format: 2.0
+                    package test.pkg {
+                      public class Foo {
+                        method @FlaggedApi("foo/bar") public void flaggedSystemApi();
+                      }
+                    }
+                """,
+            expectedSystemApiMinusFlaggedApi =
+                """
+                    // Signature format: 2.0
+                """,
         )
     }
 }
