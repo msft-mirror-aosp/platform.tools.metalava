2fd0089305f1e4da35431de88eca3fb018314a4b
Julia McClellan <juliamcclellan@google.com>
2023-11-15 13:28:38 -0500

    Add FileFormat property for Kotlin name/type ordering
    As described in https://docs.google.com/document/d/1uEsujCuZY-l9pVRg1ETbeiSoJK4iO5i9RyhsnU2Sz-c/edit
    
    To be implemented in followup CLs.
    
    Bug: 307926200
    Test: Updated tests, added a basic test of input/output for the new
    format property with a simple API.
    
    Change-Id: I5c9f467a6e815842a1d5092be77f2bebe57843b5

---
+++ metalava/src/test/java/com/android/tools/metalava/SignatureInputOutputTest.kt
@@ -0,0 +1,94 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.tools.metalava
+
+import com.android.tools.metalava.model.Codebase
+import com.android.tools.metalava.model.text.ApiFile
+import com.android.tools.metalava.model.text.FileFormat
+import com.android.tools.metalava.model.text.assertSignatureFilesMatch
+import com.android.tools.metalava.model.visitors.ApiVisitor
+import com.google.common.truth.Truth.assertThat
+import java.io.PrintWriter
+import java.io.StringWriter
+import org.junit.Test
+
+class SignatureInputOutputTest {
+    /**
+     * Parses the API (without a header line, the header from [format] will be added) from the
+     * [signature], runs the [codebaseTest] on the parsed codebase, and then writes the codebase
+     * back out in the [format], verifying that the output matches the original [signature].
+     *
+     * This tests both [ApiFile] and [SignatureWriter].
+     */
+    private fun runInputOutputTest(
+        signature: String,
+        format: FileFormat,
+        codebaseTest: (Codebase) -> Unit
+    ) {
+        val fullSignature = format.header() + signature
+        val codebase = ApiFile.parseApi("test", fullSignature)
+
+        codebaseTest(codebase)
+
+        val output =
+            StringWriter().use { stringWriter ->
+                PrintWriter(stringWriter).use { printWriter ->
+                    val signatureWriter =
+                        SignatureWriter(
+                            writer = printWriter,
+                            filterEmit = { true },
+                            filterReference = { true },
+                            preFiltered = false,
+                            emitHeader = EmitFileHeader.IF_NONEMPTY_FILE,
+                            fileFormat = format,
+                            showUnannotated = false,
+                            apiVisitorConfig = ApiVisitor.Config(),
+                        )
+                    codebase.accept(signatureWriter)
+                }
+                stringWriter.toString()
+            }
+
+        assertSignatureFilesMatch(signature, output, format)
+    }
+
+    @Test
+    fun `Test basic signature file`() {
+        val api =
+            """
+                package test.pkg {
+                  public class Foo {
+                    ctor public Foo();
+                  }
+                }
+            """
+                .trimIndent()
+
+        runInputOutputTest(api, kotlinStyleFormat) { codebase ->
+            val foo = codebase.findClass("test.pkg.Foo")
+            assertThat(foo).isNotNull()
+            assertThat(foo!!.constructors()).hasSize(1)
+            val ctor = foo.constructors().single()
+            assertThat(ctor.parameters()).isEmpty()
+        }
+    }
+
+    companion object {
+        private val kotlinStyleFormat =
+            FileFormat.V5.copy(kotlinNameTypeOrder = true, formatDefaults = FileFormat.V5)
+    }
+}
