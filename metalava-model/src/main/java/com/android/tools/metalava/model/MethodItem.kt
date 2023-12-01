48cd9aa95cebae01f2c9f0fb058f4bb10ee9433e
Julia McClellan <juliamcclellan@google.com>
2023-12-01 10:22:31 -0500

    Remove ClassItem and MethodItem typeArgumentClasses
    All usages have been replaced.
    
    Bug: 300653498
    Test: N/A
    
    Change-Id: Ic80c00791c83fad2ef1cc4bae3bced43f34d50a6

--- metalava-model/src/main/java/com/android/tools/metalava/model/MethodItem.kt
+++ metalava-model/src/main/java/com/android/tools/metalava/model/MethodItem.kt
@@ -61,9 +61,6 @@ interface MethodItem : MemberItem {
      */
     @MetalavaApi fun typeParameterList(): TypeParameterList
 
-    /** Returns the classes that are part of the type parameters of this method, if any */
-    fun typeArgumentClasses(): List<ClassItem> = codebase.unsupported()
-
     /** Types of exceptions that this method can throw */
     fun throwsTypes(): List<ClassItem>
 
