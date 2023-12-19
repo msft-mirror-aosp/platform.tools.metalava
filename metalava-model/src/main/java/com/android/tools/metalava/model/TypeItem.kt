302340213b120c2a8431e181bdae843895964d4e
Julia McClellan <juliamcclellan@google.com>
2023-12-19 15:46:29 -0500

    Use kotlin-nulls in new toTypeString
    Except for the psi model. Updating the psi model to use the new
    version of the method requires a lot of AndroidX signature file
    updates (see aosp/2882306) and will need to be feature flagged.
    
    Bug: 309149849
    Test: CommonTypeStringTest, updates some previously incorrect tests
    Change-Id: Ifa54e1665a26d96c1ab763154da91df37997a2f0

--- metalava-model/src/main/java/com/android/tools/metalava/model/TypeItem.kt
+++ metalava-model/src/main/java/com/android/tools/metalava/model/TypeItem.kt
@@ -480,6 +480,7 @@ abstract class DefaultTypeItem(private val codebase: Codebase) : TypeItem {
                         appendAnnotations(type.modifiers, configuration)
                     }
                     append(type.kind.primitiveName)
+                    // Primitives must be non-null.
                 }
                 is ArrayTypeItem -> {
                     // The ordering of array annotations means this can't just use a recursive
@@ -492,18 +493,22 @@ abstract class DefaultTypeItem(private val codebase: Codebase) : TypeItem {
                             arrayModifiers.add(deepComponentType.modifiers)
                             deepComponentType = deepComponentType.componentType
                         }
+                        val suffixes = arrayModifiers.map { it.nullability().suffix }.reversed()
 
                         // Print the innermost component type.
                         appendTypeString(deepComponentType, configuration)
 
                         // Print modifiers from the outermost array type in, and the array suffixes.
-                        arrayModifiers.forEachIndexed { index, modifiers ->
+                        arrayModifiers.zip(suffixes).forEachIndexed { index, (modifiers, suffix) ->
                             appendAnnotations(modifiers, configuration, leadingSpace = true)
                             // Only the outermost array can be varargs.
                             if (index < arrayModifiers.size - 1 || !type.isVarargs) {
                                 append("[]")
                             } else {
                                 append("...")
+                            }
+                            if (configuration.kotlinStyleNulls) {
+                                append(suffix)
                             }
                         }
                     } else {
@@ -514,8 +519,10 @@ abstract class DefaultTypeItem(private val codebase: Codebase) : TypeItem {
                         } else {
                             append("[]")
                         }
+                        if (configuration.kotlinStyleNulls) {
+                            append(type.modifiers.nullability().suffix)
+                        }
                     }
-                    // TODO: kotlin nulls
                 }
                 is ClassTypeItem -> {
                     if (type.outerClassType != null) {
@@ -548,14 +555,18 @@ abstract class DefaultTypeItem(private val codebase: Codebase) : TypeItem {
                         }
                         append(">")
                     }
-                    // TODO: kotlin nulls
+                    if (configuration.kotlinStyleNulls) {
+                        append(type.modifiers.nullability().suffix)
+                    }
                 }
                 is VariableTypeItem -> {
                     if (configuration.annotations) {
                         appendAnnotations(type.modifiers, configuration)
                     }
                     append(type.name)
-                    // TODO: kotlin nulls
+                    if (configuration.kotlinStyleNulls) {
+                        append(type.modifiers.nullability().suffix)
+                    }
                 }
                 is WildcardTypeItem -> {
                     if (configuration.annotations) {
@@ -573,6 +584,8 @@ abstract class DefaultTypeItem(private val codebase: Codebase) : TypeItem {
                         append(" super ")
                         appendTypeString(it, configuration)
                     }
+                    // It doesn't make sense to have a nullness suffix on a wildcard, this should be
+                    // handled by the bound.
                 }
             }
         }
@@ -585,6 +598,11 @@ abstract class DefaultTypeItem(private val codebase: Codebase) : TypeItem {
         ) {
             val annotations =
                 modifiers.annotations().filter { annotation ->
+                    // If Kotlin-style nulls are printed, nullness annotations shouldn't be.
+                    if (configuration.kotlinStyleNulls && annotation.isNullnessAnnotation()) {
+                        return@filter false
+                    }
+
                     val filter = configuration.filter ?: return@filter true
                     val qualifiedName = annotation.qualifiedName ?: return@filter true
                     val annotationClass =
