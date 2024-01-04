17143074a7969f0bc028cbadb432e2ad449cf3de
Julia McClellan <juliamcclellan@google.com>
2024-01-04 11:20:42 -0500

    Make toType for type parameters return a variable type
    It previously returned a class type for the turbine and text models.
    
    Bug: 301285165
    Test: CommonTypeParameterItemTest
    Change-Id: Ib6470dc3ab98f4f26df1a540ad8815fdee462ac6

--- metalava-model-text/src/main/java/com/android/tools/metalava/model/text/TextTypeParameterItem.kt
+++ metalava-model-text/src/main/java/com/android/tools/metalava/model/text/TextTypeParameterItem.kt
@@ -39,6 +39,16 @@ class TextTypeParameterItem(
     ),
     TypeParameterItem {
 
+    override fun toType(): TextTypeItem {
+        return TextVariableTypeItem(
+            codebase,
+            name,
+            name,
+            this,
+            TextTypeModifiers.create(codebase, emptyList(), null)
+        )
+    }
+
     override fun typeBounds(): List<TypeItem> {
         if (bounds == null) {
             val boundsStringList = bounds(typeParameterString, owner)
