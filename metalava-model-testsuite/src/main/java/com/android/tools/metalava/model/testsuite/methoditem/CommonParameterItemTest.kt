c230bea16872547b03ef4c2a2005661b6e028c16
Paul Duffin <paulduffin@google.com>
2024-01-19 10:43:46 +0000

    Add model tests for ParameterItem.name/publicName
    Bug: 321109677
    Test: ./gradlew
    Change-Id: Ifbbe246f2e1901237cb98bbe7e4e74bf89407b9a

--- metalava-model-testsuite/src/main/java/com/android/tools/metalava/model/testsuite/methoditem/CommonParameterItemTest.kt
+++ metalava-model-testsuite/src/main/java/com/android/tools/metalava/model/testsuite/methoditem/CommonParameterItemTest.kt
@@ -18,9 +18,11 @@ package com.android.tools.metalava.model.testsuite.methoditem
 
 import com.android.tools.metalava.model.source.SourceLanguage
 import com.android.tools.metalava.model.testsuite.BaseModelTest
+import com.android.tools.metalava.testing.KnownSourceFiles
 import com.android.tools.metalava.testing.java
 import com.android.tools.metalava.testing.kotlin
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
@@ -110,6 +112,108 @@ class CommonParameterItemTest : BaseModelTest() {
                 codebase.assertClass("test.pkg.Bar").methods().single().parameters().single()
             assertEquals("deprecated", false, parameterItem.deprecated)
             assertEquals("originallyDeprecated", false, parameterItem.originallyDeprecated)
+        }
+    }
+
+    @Test
+    fun `Test publicName reports correct name when specified`() {
+        runCodebaseTest(
+            inputSet(
+                signature(
+                    """
+                        // Signature format: 2.0
+                        package test.pkg {
+                          public class Bar {
+                            method public void foo(int baz);
+                          }
+                        }
+                    """
+                ),
+            ),
+            inputSet(
+                KnownSourceFiles.supportParameterName,
+                java(
+                    """
+                        package test.pkg;
+
+                        import androidx.annotation.ParameterName;
+
+                        public class Bar {
+                            public void foo(@ParameterName("baz") int baz) {}
+                        }
+                    """
+                ),
+            ),
+            inputSet(
+                kotlin(
+                    """
+                        package test.pkg
+
+                        class Bar {
+                            fun foo(baz: Int) {}
+                        }
+                    """
+                ),
+            ),
+        ) {
+            val parameterItem =
+                codebase.assertClass("test.pkg.Bar").methods().single().parameters().single()
+            assertEquals("name()", "baz", parameterItem.name())
+            assertEquals("publicName()", "baz", parameterItem.publicName())
+        }
+    }
+
+    @Test
+    fun `Test publicName reports correct name when not specified`() {
+        runCodebaseTest(
+            signature(
+                """
+                    // Signature format: 2.0
+                    package test.pkg {
+                      public class Bar {
+                        method public void foo(int);
+                      }
+                    }
+                """
+            ),
+            java(
+                """
+                    package test.pkg;
+
+                    public class Bar {
+                        public void foo(int baz) {}
+                    }
+                """
+            ),
+            // Kotlin treats all parameter names as public.
+        ) {
+            val parameterItem =
+                codebase.assertClass("test.pkg.Bar").methods().single().parameters().single()
+            assertNull("publicName()", parameterItem.publicName())
+        }
+    }
+
+    @Test
+    fun `Test publicName reports correct name when called on binary class`() {
+        runCodebaseTest(
+            java(
+                """
+                    package test.pkg;
+
+                    public class Bar {
+                    }
+                """
+            ),
+            // Kotlin treats all parameter names as public.
+        ) {
+            val parameterItem =
+                codebase
+                    .assertClass("java.lang.Object")
+                    .assertMethod("equals", "java.lang.Object")
+                    .parameters()
+                    .single()
+            assertEquals("name()", "obj", parameterItem.name())
+            assertEquals("publicName()", "obj", parameterItem.publicName())
         }
     }
 }
