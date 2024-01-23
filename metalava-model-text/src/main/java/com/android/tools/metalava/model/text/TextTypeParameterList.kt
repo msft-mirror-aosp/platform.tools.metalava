9912e89ea3a6344e203cd226ac8c1c1a82d6db76
Paul Duffin <paulduffin@google.com>
2024-01-23 15:40:13 +0000

    Kotlinize typeParameters creation
    Bug: 321915411
    Test: ./gradlew
    Change-Id: I1401127f5ac959c1f7074e3f82593a4991ddd3da

--- metalava-model-text/src/main/java/com/android/tools/metalava/model/text/TextTypeParameterList.kt
+++ metalava-model-text/src/main/java/com/android/tools/metalava/model/text/TextTypeParameterList.kt
@@ -32,9 +32,7 @@ class TextTypeParameterList(
     override fun typeParameters(): List<TypeParameterItem> {
         if (typeParameters == null) {
             val strings = TextTypeParser.typeParameterStrings(typeListString)
-            val list = ArrayList<TextTypeParameterItem>(strings.size)
-            strings.mapTo(list) { TextTypeParameterItem.create(codebase, owner, it) }
-            typeParameters = list
+            typeParameters = strings.map { TextTypeParameterItem.create(codebase, owner, it) }
         }
         return typeParameters!!
     }
