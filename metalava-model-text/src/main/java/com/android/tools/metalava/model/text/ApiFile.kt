ca710313c0ce145cf6367bbe3a9e2387b4490332
Paul Duffin <paulduffin@google.com>
2024-01-27 01:32:55 +0000

    Kotlinize ReferenceResolver.resolveThrowsClasses()
    Bug: 322499518
    Test: ./gradlew
    Change-Id: I62e3321e43e1511e11afd5f8a2555c94a8cd5f02

--- metalava-model-text/src/main/java/com/android/tools/metalava/model/text/ApiFile.kt
+++ metalava-model-text/src/main/java/com/android/tools/metalava/model/text/ApiFile.kt
@@ -1412,32 +1412,35 @@ internal class ReferenceResolver(
         val methodInfo = methodItem as TextMethodItem
         val names = methodInfo.throwsTypeNames()
         if (names.isNotEmpty()) {
-            val result = ArrayList<ClassItem>()
-            for (exception in names) {
-                var exceptionClass: ClassItem? = codebase.mAllClasses[exception]
-                if (exceptionClass == null) {
-                    // Exception not provided by this codebase. Either try and retrieve it from a
-                    // base codebase or create a stub.
-                    exceptionClass = codebase.getOrCreateClass(exception)
-
-                    // A class retrieved from another codebase is assumed to have been fully
-                    // resolved by the codebase. However, a stub that has just been created will
-                    // need some additional work. A stub can be differentiated from a ClassItem
-                    // retrieved from another codebase because it belongs to this codebase and is
-                    // a TextClassItem.
-                    if (exceptionClass.codebase == codebase && exceptionClass is TextClassItem) {
-                        // An exception class needs to extend Throwable, unless it is Throwable in
-                        // which case it does not need modifying.
-                        if (exception != JAVA_LANG_THROWABLE) {
-                            val throwableClass = codebase.getOrCreateClass(JAVA_LANG_THROWABLE)
-                            exceptionClass.setSuperClass(throwableClass, throwableClass.toType())
-                        }
-                    }
+            val throwsList =
+                names.map { exception ->
+                    // Search in this codebase, then fall back to searching in a base codebase and
+                    // finally creating a stub.
+                    codebase.findClass(exception) ?: getOrCreateThrowableClass(exception)
                 }
-                result.add(exceptionClass)
-            }
-            methodInfo.setThrowsList(result)
+            methodInfo.setThrowsList(throwsList)
         }
+    }
+
+    private fun getOrCreateThrowableClass(exception: String): ClassItem {
+        // Exception not provided by this codebase. Either try and retrieve it from a base codebase
+        // or create a stub.
+        val exceptionClass = codebase.getOrCreateClass(exception)
+
+        // A class retrieved from another codebase is assumed to have been fully resolved by the
+        // codebase. However, a stub that has just been created will need some additional work. A
+        // stub can be differentiated from a ClassItem retrieved from another codebase because it
+        // belongs to this codebase and is a TextClassItem.
+        if (exceptionClass.codebase == codebase && exceptionClass is TextClassItem) {
+            // An exception class needs to extend Throwable, unless it is Throwable in
+            // which case it does not need modifying.
+            if (exception != JAVA_LANG_THROWABLE) {
+                val throwableClass = codebase.getOrCreateClass(JAVA_LANG_THROWABLE)
+                exceptionClass.setSuperClass(throwableClass, throwableClass.toType())
+            }
+        }
+
+        return exceptionClass
     }
 
     private fun resolveInnerClasses() {
