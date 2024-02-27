09fb1255a7f70fefb736cc5696ac582fc04770ce
Paul Duffin <paulduffin@google.com>
2024-02-27 01:32:58 +0000

    Run main metalava stubs tests against Turbine
    For now only subclasses of `AbstractStubTest` are run against Turbine.
    
    Bug: 326649406
    Test: ./gradlew
    Change-Id: I20c3ddcbae6d0c5bd3f818ed237804263a44ae39

--- metalava/src/test/java/com/android/tools/metalava/DriverTest.kt
+++ metalava/src/test/java/com/android/tools/metalava/DriverTest.kt
@@ -46,6 +46,8 @@ import com.android.tools.metalava.model.source.SourceModelProvider
 import com.android.tools.metalava.model.source.SourceSet
 import com.android.tools.metalava.model.testing.CodebaseCreatorConfig
 import com.android.tools.metalava.model.testing.CodebaseCreatorConfigAware
+import com.android.tools.metalava.model.testing.FilterByProvider
+import com.android.tools.metalava.model.testing.FilterByProvider.FilterAction.EXCLUDE
 import com.android.tools.metalava.model.text.ApiClassResolution
 import com.android.tools.metalava.model.text.ApiFile
 import com.android.tools.metalava.model.text.FileFormat
@@ -81,6 +83,7 @@ import org.junit.rules.TemporaryFolder
 import org.junit.runner.RunWith
 
 @RunWith(DriverTestRunner::class)
+@FilterByProvider(provider = "turbine", action = EXCLUDE)
 abstract class DriverTest : CodebaseCreatorConfigAware<SourceModelProvider>, TemporaryFolderOwner {
     @get:Rule override val temporaryFolder = TemporaryFolder()
 
