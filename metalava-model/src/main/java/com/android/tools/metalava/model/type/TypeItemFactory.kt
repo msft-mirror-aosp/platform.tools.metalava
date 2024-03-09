258be2136881f5a84b262412d39cffb7b4ea066b
Paul Duffin <paulduffin@google.com>
2024-03-09 00:40:42 +0000

    Cleanup documentation and unnecessary code for field types
    The previous change to add `TypeItemFactory.getFieldType(...)` was
    slightly incomplete, as it was missing some documentation and did not
    remove `PsiPropertyItem.finishInitialization()` which was no longer
    needed. This change fixes that.
    
    Bug: 325454211
    Test: ./gradlew
    Change-Id: Ia4ffeb3c37edbddbbb703ba78380343b1a848667

--- metalava-model/src/main/java/com/android/tools/metalava/model/type/TypeItemFactory.kt
+++ metalava-model/src/main/java/com/android/tools/metalava/model/type/TypeItemFactory.kt
@@ -88,12 +88,14 @@ interface TypeItemFactory<in T, F : TypeItemFactory<T, F>> {
      *
      * This considers a number of factors, in addition to the declared type, to determine the
      * appropriate [TypeNullability] for the field type, i.e.:
+     * * Any [AnnotationItem.typeNullability] annotations in [itemAnnotations].
      * * Setting of [isEnumConstant]; if it is `true` then it is always [TypeNullability.NONNULL].
      * * If the field is `final` then the nullability of the field's value can be considered
      *   ([isInitialValueNonNull]). Otherwise, it cannot as it may change over the lifetime of the
      *   field.
      *
      * @param underlyingType the underlying model's type.
+     * @param itemAnnotations the annotations on the field (not the type).
      * @param isFinal `true` if the field is `final`.
      * @param isEnumConstant `true` if the field is actually an enum constant.
      * @param isInitialValueNonNull a lambda that will be invoked on `final` fields to determine
