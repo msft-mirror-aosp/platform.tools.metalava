4134e2f16c04ff6d161d6c469165d385fa5682a5
Paul Duffin <paulduffin@google.com>
2024-05-30 10:15:42 +0100

    Report UnflaggedApi issue on modifier changes to existing APIs
    Previously, changes to modifiers of existing APIs were not required to
    be annotated with `@FlaggedApi`. This change extends the existing
    `UnflaggedApi` issue to require `@FlaggedApi` on changes to an
    existing API's modifiers. The `UnflaggedApi` issues are reported as
    `Severity.WARNING_ERROR_WHEN_NEW` until the existing issues are fixed.
    
    Supporting that required a number of other changes:
    1. The `computeMaximumSeverity(Item)` needed to be passed the `Issue`
       being reported so that it would not suppress `UnflaggedApi` issues.
    2. The `ApiLint.report(...)` method needed a `maximumSeverity`
       parameter to allow the caller to reduce the configured severity of
       the issue.
    
    Bug: 339166506
    Test: ./gradlew
    Change-Id: Ia955eebae29053d144be69e6e585c6236d2cfea7

--- metalava/src/test/java/com/android/tools/metalava/lint/FlaggedApiLintTest.kt
+++ metalava/src/test/java/com/android/tools/metalava/lint/FlaggedApiLintTest.kt
@@ -498,7 +498,12 @@ class FlaggedApiLintTest : DriverTest() {
     @Test
     fun `Require @FlaggedApi on APIs whose modifiers have changed`() {
         check(
-            expectedIssues = "",
+            expectedIssues =
+                """
+                    src/test/pkg/Foo.java:3: warning: Changes to modifiers, from 'public abstract' to 'public' must be flagged with @FlaggedApi: class test.pkg.Foo [UnflaggedApi]
+                    src/test/pkg/Foo.java:4: warning: Changes to modifiers, from 'protected' to 'public' must be flagged with @FlaggedApi: constructor test.pkg.Foo() [UnflaggedApi]
+                    src/test/pkg/Foo.java:5: warning: Changes to modifiers, from 'public final' to 'public' must be flagged with @FlaggedApi: method test.pkg.Foo.method() [UnflaggedApi]
+                """,
             apiLint =
                 """
                     // Signature format: 2.0
