43d57eedcea2739ff65c6b8351748bd09fe07a67
Paul Duffin <paulduffin@google.com>
2024-06-11 12:05:35 +0100

    Remove unnecessary deprecation attributes in api-versions.xml
    Previously, members of a deprecated class would have a "deprecated"
    attribute that reported that they were deprecated in the same API
    version as the class (if they were not deprecated prior to that). That
    is unnecessary as the code that consumes the `api-versions.xml` file
    will use the class's deprecated API version for members if the members
    do not specify their own version.
    
    This change will only add the "deprecated" attribute if it is
    different to the parent element's reducing the size of the generated
    `api-versions.xml` file.
    
    Bug: 315206833
    Test: ./gradlew
    Change-Id: I7affaf7a32625e51b5635225e30446dddcc3d34f

--- metalava/src/test/java/com/android/tools/metalava/apilevels/ApiGeneratorTest.kt
+++ metalava/src/test/java/com/android/tools/metalava/apilevels/ApiGeneratorTest.kt
@@ -100,12 +100,12 @@ class ApiGeneratorTest : DriverTest() {
                 <sdk id="31" shortname="S-ext" name="S Extensions" reference="android/os/Build${'$'}VERSION_CODES${'$'}S"/>
                 <class name="android/test/ClassAddedAndDeprecatedInApi30" since="30" deprecated="30">
                 <extends name="java/lang/Object"/>
-                <method name="&lt;init>(F)V" deprecated="30"/>
-                <method name="&lt;init>(I)V" deprecated="30"/>
-                <method name="methodExplicitlyDeprecated()V" deprecated="30"/>
-                <method name="methodImplicitlyDeprecated()V" deprecated="30"/>
-                <field name="FIELD_EXPLICITLY_DEPRECATED" deprecated="30"/>
-                <field name="FIELD_IMPLICITLY_DEPRECATED" deprecated="30"/>
+                <method name="&lt;init>(F)V"/>
+                <method name="&lt;init>(I)V"/>
+                <method name="methodExplicitlyDeprecated()V"/>
+                <method name="methodImplicitlyDeprecated()V"/>
+                <field name="FIELD_EXPLICITLY_DEPRECATED"/>
+                <field name="FIELD_IMPLICITLY_DEPRECATED"/>
                 </class>
                 <class name="android/test/ClassAddedInApi30" since="30">
                     <extends name="java/lang/Object"/>
@@ -188,12 +188,12 @@ class ApiGeneratorTest : DriverTest() {
                 <sdk id="31" shortname="S-ext" name="S Extensions" reference="android/os/Build${'$'}VERSION_CODES${'$'}S"/>
                 <class name="android/test/ClassAddedAndDeprecatedInApi30" since="30" deprecated="30">
                 <extends name="java/lang/Object"/>
-                <method name="&lt;init>(F)V" deprecated="30"/>
-                <method name="&lt;init>(I)V" deprecated="30"/>
-                <method name="methodExplicitlyDeprecated()V" deprecated="30"/>
-                <method name="methodImplicitlyDeprecated()V" deprecated="30"/>
-                <field name="FIELD_EXPLICITLY_DEPRECATED" deprecated="30"/>
-                <field name="FIELD_IMPLICITLY_DEPRECATED" deprecated="30"/>
+                <method name="&lt;init>(F)V"/>
+                <method name="&lt;init>(I)V"/>
+                <method name="methodExplicitlyDeprecated()V"/>
+                <method name="methodImplicitlyDeprecated()V"/>
+                <field name="FIELD_EXPLICITLY_DEPRECATED"/>
+                <field name="FIELD_IMPLICITLY_DEPRECATED"/>
                 </class>
                 <class name="android/test/ClassAddedInApi30" since="30">
                     <extends name="java/lang/Object"/>
