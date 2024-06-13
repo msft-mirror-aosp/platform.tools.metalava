d87ab005b73ed3829b8c94237537b6bba13ac570
Paul Duffin <paulduffin@google.com>
2024-06-13 01:21:13 +0100

    Dedup superClass() implementation
    Adds a default implementation in terms of `superClassType()?.asClass()`
    and removes the module specific, but equivalent, implementations.
    
    Bug: 346745741
    Test: ./gradlew
    Change-Id: Ida7d07be7ff6425612f9086de4ecaf643f6f2887

--- metalava-model-text/src/main/java/com/android/tools/metalava/model/text/TextClassItem.kt
+++ metalava-model-text/src/main/java/com/android/tools/metalava/model/text/TextClassItem.kt
@@ -100,8 +100,6 @@ internal open class TextClassItem(
 
     private var superClassType: ClassTypeItem? = null
 
-    override fun superClass(): ClassItem? = superClassType?.asClass()
-
     override fun superClassType(): ClassTypeItem? = superClassType
 
     internal fun setSuperClassType(superClassType: ClassTypeItem?) {
