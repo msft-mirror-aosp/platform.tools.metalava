c5e44cacdb3416a0bf0f5d713798d932cb0dd390
Paul Duffin <paulduffin@google.com>
2024-07-11 11:51:47 +0100

    Add CallableItem as a super interface of MethodItem
    Moves methods from `MethodItem` that clearly also apply to
    `ConstructorItem`. It is possible that other methods may need moving in
    future. This purposely kept the same order so as to make it easier to
    diff against the original `MethodItem` and does not change any "method"
    references. They will be done in a follow up change where it will be
    easier to see the changes.
    
    Bug: 352481366
    Test: ./gradlew
    Change-Id: Ia322f7ad48114cec497b33403c8c7acc4162ec21

--- metalava-model/src/main/java/com/android/tools/metalava/model/MethodItem.kt
+++ metalava-model/src/main/java/com/android/tools/metalava/model/MethodItem.kt
@@ -16,11 +16,10 @@
 
 package com.android.tools.metalava.model
 
-import java.util.Objects
 import java.util.function.Predicate
 
 @MetalavaApi
-interface MethodItem : MemberItem, InheritableItem, TypeParameterListOwner {
+interface MethodItem : CallableItem, InheritableItem {
     /**
      * The property this method is an accessor for; inverse of [PropertyItem.getter] and
      * [PropertyItem.setter]
@@ -28,22 +27,11 @@ interface MethodItem : MemberItem, InheritableItem, TypeParameterListOwner {
     val property: PropertyItem?
         get() = null
 
-    /** Whether this method is a constructor */
-    @MetalavaApi fun isConstructor(): Boolean
-
-    /** The type of this field. Returns the containing class for constructors */
-    @MetalavaApi fun returnType(): TypeItem
-
-    /** The list of parameters */
-    @MetalavaApi fun parameters(): List<ParameterItem>
-
     /** Returns true if this method is a Kotlin extension method */
     fun isExtensionMethod(): Boolean
 
     /** Returns the super methods that this method is overriding */
     fun superMethods(): List<MethodItem>
-
-    override fun type() = returnType()
 
     override fun findCorrespondingItemIn(
         codebase: Codebase,
@@ -72,52 +60,6 @@ interface MethodItem : MemberItem, InheritableItem, TypeParameterListOwner {
             val superMethods = item.superMethods()
             superMethods.firstOrNull()
         }
-    }
-
-    /** Types of exceptions that this method can throw */
-    fun throwsTypes(): List<ExceptionTypeItem>
-
-    /** Returns true if this method throws the given exception */
-    fun throws(qualifiedName: String): Boolean {
-        for (type in throwsTypes()) {
-            val throwableClass = type.erasedClass ?: continue
-            if (throwableClass.extends(qualifiedName)) {
-                return true
-            }
-        }
-
-        return false
-    }
-
-    fun filteredThrowsTypes(predicate: Predicate<Item>): Collection<ExceptionTypeItem> {
-        if (throwsTypes().isEmpty()) {
-            return emptyList()
-        }
-        return filteredThrowsTypes(predicate, LinkedHashSet())
-    }
-
-    private fun filteredThrowsTypes(
-        predicate: Predicate<Item>,
-        throwsTypes: LinkedHashSet<ExceptionTypeItem>
-    ): LinkedHashSet<ExceptionTypeItem> {
-        for (exceptionType in throwsTypes()) {
-            if (exceptionType is VariableTypeItem) {
-                throwsTypes.add(exceptionType)
-            } else {
-                val classItem = exceptionType.erasedClass ?: continue
-                if (predicate.test(classItem)) {
-                    throwsTypes.add(exceptionType)
-                } else {
-                    // Excluded, but it may have super class throwables that are included; if so,
-                    // include those.
-                    classItem
-                        .allSuperClasses()
-                        .firstOrNull { superClass -> predicate.test(superClass) }
-                        ?.let { superClass -> throwsTypes.add(superClass.type()) }
-                }
-            }
-        }
-        return throwsTypes
     }
 
     /**
@@ -154,22 +96,8 @@ interface MethodItem : MemberItem, InheritableItem, TypeParameterListOwner {
         return null
     }
 
-    override fun baselineElementId() = buildString {
-        append(containingClass().qualifiedName())
-        append("#")
-        append(name())
-        append("(")
-        parameters().joinTo(this) { it.type().toSimpleType() }
-        append(")")
-    }
-
     override fun accept(visitor: ItemVisitor) {
         visitor.visit(this)
-    }
-
-    override fun toStringForItem(): String {
-        return "${if (isConstructor()) "constructor" else "method"} ${
-            containingClass().qualifiedName()}.${name()}(${parameters().joinToString { it.type().toSimpleType() }})"
     }
 
     companion object {
@@ -355,143 +283,11 @@ interface MethodItem : MemberItem, InheritableItem, TypeParameterListOwner {
         }
     }
 
-    /**
-     * Finds uncaught exceptions actually thrown inside this method (as opposed to ones declared in
-     * the signature)
-     */
-    fun findThrownExceptions(): Set<ClassItem> = codebase.unsupported()
-
     /** If annotation method, returns the default value as a source expression */
     fun defaultValue(): String = ""
 
     fun hasDefaultValue(): Boolean {
         return defaultValue() != ""
-    }
-
-    /**
-     * Returns true if overloads of the method should be checked separately when checking signature
-     * of the method.
-     *
-     * This works around the issue of actual method not generating overloads for @JvmOverloads
-     * annotation when the default is specified on expect side
-     * (https://youtrack.jetbrains.com/issue/KT-57537).
-     */
-    fun shouldExpandOverloads(): Boolean = false
-
-    override fun equalsToItem(other: Any?): Boolean {
-        if (this === other) return true
-        if (other !is MethodItem) return false
-
-        if (name() != other.name()) {
-            return false
-        }
-
-        if (containingClass() != other.containingClass()) {
-            return false
-        }
-
-        val parameters1 = parameters()
-        val parameters2 = other.parameters()
-
-        if (parameters1.size != parameters2.size) {
-            return false
-        }
-
-        for (i in parameters1.indices) {
-            val parameter1 = parameters1[i]
-            val parameter2 = parameters2[i]
-            if (parameter1.type() != parameter2.type()) {
-                return false
-            }
-        }
-
-        val typeParameters1 = typeParameterList
-        val typeParameters2 = other.typeParameterList
-
-        if (typeParameters1.size != typeParameters2.size) {
-            return false
-        }
-
-        for (i in typeParameters1.indices) {
-            val typeParameter1 = typeParameters1[i]
-            val typeParameter2 = typeParameters2[i]
-            if (typeParameter1.typeBounds() != typeParameter2.typeBounds()) {
-                return false
-            }
-        }
-        return true
-    }
-
-    override fun hashCodeForItem(): Int {
-        // Just use the method name, containing class and number of parameters.
-        return Objects.hash(name(), containingClass(), parameters().size)
-    }
-
-    /**
-     * Returns true if this method is a signature match for the given method (e.g. can be
-     * overriding). This checks that the name and parameter lists match, but ignores differences in
-     * parameter names, return value types and throws list types.
-     */
-    fun matches(other: MethodItem): Boolean {
-        if (this === other) return true
-
-        if (name() != other.name()) {
-            return false
-        }
-
-        val parameters1 = parameters()
-        val parameters2 = other.parameters()
-
-        if (parameters1.size != parameters2.size) {
-            return false
-        }
-
-        for (i in parameters1.indices) {
-            val parameter1Type = parameters1[i].type()
-            val parameter2Type = parameters2[i].type()
-            if (parameter1Type == parameter2Type) continue
-            if (parameter1Type.toErasedTypeString() == parameter2Type.toErasedTypeString()) continue
-
-            val convertedType =
-                parameter1Type.convertType(other.containingClass(), containingClass())
-            if (convertedType != parameter2Type) return false
-        }
-        return true
-    }
-
-    /**
-     * Returns whether this method has any types in its signature that does not match the given
-     * filter
-     */
-    fun hasHiddenType(filterReference: Predicate<Item>): Boolean {
-        for (parameter in parameters()) {
-            if (parameter.type().hasHiddenType(filterReference)) return true
-        }
-
-        if (returnType().hasHiddenType(filterReference)) return true
-
-        for (typeParameter in typeParameterList) {
-            if (typeParameter.typeBounds().any { it.hasHiddenType(filterReference) }) return true
-        }
-
-        return false
-    }
-
-    /** Checks if there is a reference to a hidden class anywhere in the type. */
-    private fun TypeItem.hasHiddenType(filterReference: Predicate<Item>): Boolean {
-        return when (this) {
-            is PrimitiveTypeItem -> false
-            is ArrayTypeItem -> componentType.hasHiddenType(filterReference)
-            is ClassTypeItem ->
-                asClass()?.let { !filterReference.test(it) } == true ||
-                    outerClassType?.hasHiddenType(filterReference) == true ||
-                    arguments.any { it.hasHiddenType(filterReference) }
-            is VariableTypeItem -> !filterReference.test(asTypeParameter)
-            is WildcardTypeItem ->
-                extendsBound?.hasHiddenType(filterReference) == true ||
-                    superBound?.hasHiddenType(filterReference) == true
-            else -> throw IllegalStateException("Unrecognized type: $this")
-        }
     }
 
     /** Whether this method is a getter/setter for an underlying Kotlin property (val/var) */
