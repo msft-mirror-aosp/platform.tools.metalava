f17ff81a0136eeb7438f0a1f21252012d2c3e32d
Paul Duffin <paulduffin@google.com>
2024-07-31 12:51:51 +0100

    Move TextModifierListTest to testsuite.CommonModifierListTest
    The test is no longer specified to the test model and is just testing
    `DefaultModifierList`, albeit with a very simple test.
    
    This is moved into `metalava-model-testsuite` as it will need to use a
    `Codebase` and other `Item` instances as part of the test.
    
    Bug: 356548977
    Test: ./gradlew
    Change-Id: I6ed2176bf41f1aed1e3e36092afe2428d51af8bc

--- metalava-model-testsuite/src/main/java/com/android/tools/metalava/model/testsuite/CommonModifierListTest.kt
+++ metalava-model-testsuite/src/main/java/com/android/tools/metalava/model/testsuite/CommonModifierListTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 The Android Open Source Project
+ * Copyright (C) 2024 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,15 +14,15 @@
  * limitations under the License.
  */
 
-package com.android.tools.metalava.model.text
+package com.android.tools.metalava.model.testsuite
 
 import com.android.tools.metalava.model.DefaultModifierList
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 import org.junit.Test
 
-/** Tests that the text model parses modifiers correctly. */
-class TextModifierListTest {
+/** Tests [DefaultModifierList] functionality. */
+class CommonModifierListTest : BaseModelTest() {
 
     @Test
     fun `test equivalentTo()`() {
