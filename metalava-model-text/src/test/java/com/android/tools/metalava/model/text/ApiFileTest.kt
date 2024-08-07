6c768a5e4574524784b2e1852aa151327871f3b1
Paul Duffin <paulduffin@google.com>
2024-08-08 00:01:00 +0100

    Make ApiFileTest.Test for current API surface more realistic
    Previously, it did not provide a `ClassResolver` which meant that any
    classes that would have been loaded from the resolver, e.g.
    `java.lang.Object` were created as stubs which is not realistic. This
    change adds a `ClassResolver`. It does not change the test behavior
    but it will be affected by a follow-up change and this makes the result
    of that change more consistent and realistic.
    
    Bug: 357833584
    Test: ./gradlew
    Change-Id: Ic84ed39d0d218abf04905cdaaf0e378501348cdd

--- metalava-model-text/src/test/java/com/android/tools/metalava/model/text/ApiFileTest.kt
+++ metalava-model-text/src/test/java/com/android/tools/metalava/model/text/ApiFileTest.kt
@@ -23,6 +23,7 @@ import com.android.tools.metalava.model.ClassKind
 import com.android.tools.metalava.model.ClassResolver
 import com.android.tools.metalava.model.Codebase
 import com.android.tools.metalava.model.Item
+import com.android.tools.metalava.testing.getAndroidJar
 import com.google.common.truth.Truth.assertThat
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
@@ -527,7 +528,9 @@ class ApiFileTest : BaseTextCodebaseTest() {
             files.map { file ->
                 SignatureFile(file, forCurrentApiSurface = file.name == "current.txt")
             }
-        val codebase = ApiFile.parseApi(signatureFiles)
+
+        val classResolver = ClassLoaderBasedClassResolver(getAndroidJar())
+        val codebase = ApiFile.parseApi(signatureFiles, classResolver = classResolver)
 
         val current = buildList {
             codebase.accept(
