f22092be59334d97d4447a31db8c2808f34b2e28
Julia McClellan <juliamcclellan@google.com>
2025-02-07 16:11:12 -0500

    Move more property creation into PsiPropertyItem
    Test: ./gradlew test
    Bug: 377733789
    Change-Id: I9bb120e1cecea1de222af7de879b14297da1c23d

--- metalava-model-psi/src/main/java/com/android/tools/metalava/model/psi/PsiPropertyItem.kt
+++ metalava-model-psi/src/main/java/com/android/tools/metalava/model/psi/PsiPropertyItem.kt
@@ -27,6 +27,9 @@ import com.android.tools.metalava.model.PropertyItem
 import com.android.tools.metalava.model.TypeItem
 import com.android.tools.metalava.model.item.DefaultPropertyItem
 import org.jetbrains.kotlin.psi.KtDeclaration
+import org.jetbrains.kotlin.psi.KtParameter
+import org.jetbrains.kotlin.psi.KtProperty
+import org.jetbrains.kotlin.psi.KtPropertyAccessor
 
 internal class PsiPropertyItem
 private constructor(
@@ -66,19 +69,21 @@ private constructor(
 
     companion object {
         /**
-         * Creates a new property item for the [ktDeclaration], given a [name] and relationships to
-         * other items.
+         * Creates a new property item for the [ktDeclaration], given relationships to other items.
          *
          * Kotlin's properties consist of up to four other declarations: Their accessor functions,
          * primary constructor parameter, and a backing field. These relationships are useful for
          * resolving documentation and exposing the model correctly in Kotlin stubs.
          *
-         * Most properties have a [getter], but those that are available through field access in
-         * Java (e.g. `const val` and [JvmField] properties) or are not accessible from Java (e.g.
+         * Most properties have a getter, but those that are available through field access in Java
+         * (e.g. `const val` and [JvmField] properties) or are not accessible from Java (e.g.
          * private properties and non-constructor value class properties) do not.
          *
-         * Mutable `var` properties usually have a [setter], but properties with a private default
+         * Mutable `var` properties usually have a setter, but properties with a private default
          * setter may use direct field access instead.
+         *
+         * The [accessors] should contain the getter and setter, if they exist. It may also contain
+         * other accessors, like data class component methods.
          *
          * Properties declared in the primary constructor of a class have an associated
          * [constructorParameter]. This relationship is important for resolving docs which may exist
@@ -92,12 +97,22 @@ private constructor(
             ktDeclaration: KtDeclaration,
             containingClass: ClassItem,
             typeItemFactory: PsiTypeItemFactory,
-            name: String,
-            getter: PsiMethodItem?,
-            setter: PsiMethodItem? = null,
+            accessors: List<PsiMethodItem>,
             constructorParameter: PsiParameterItem? = null,
             backingField: PsiFieldItem? = null,
         ): PsiPropertyItem? {
+            val name = ktDeclaration.name ?: return null
+            // Compute the type of the receiver, if there is one. This will be used to find the
+            // right accessors for the property.
+            val receiverType =
+                (ktDeclaration as? KtProperty)?.receiverTypeReference?.let {
+                    typeItemFactory.getTypeForKtElement(it)
+                }
+
+            // Determine which accessors are the getter and setter.
+            val getter = findGetter(accessors, receiverType)
+            val setter = findSetter(accessors, receiverType)
+
             val type =
                 getter?.returnType()
                     ?: typeItemFactory.getTypeForKtElement(ktDeclaration) ?: return null
@@ -128,6 +143,57 @@ private constructor(
             constructorParameter?.property = property
             backingField?.property = property
             return property
+        }
+
+        /**
+         * Given [allAccessors] for a property ([PsiMethodItem]s] for which the source element is
+         * the same [KtProperty]/[KtParameter]), finds the getter.
+         */
+        private fun findGetter(
+            allAccessors: List<PsiMethodItem>,
+            propertyReceiverType: PsiTypeItem?
+        ): PsiMethodItem? {
+            return if (propertyReceiverType == null) {
+                // No receiver, so the getter has no parameter. Make sure not to find a data class
+                // component method.
+                allAccessors.singleOrNull {
+                    it.parameters().isEmpty() && !it.name().startsWith("component")
+                }
+            } else {
+                // If there's a receiver, the getter should have the receiver type as its parameter.
+                allAccessors.singleOrNull {
+                    it.parameters().singleOrNull()?.type() == propertyReceiverType
+                }
+                // Work around a psi bug where value class extension property accessors don't
+                // include the receiver (b/385148821). This strategy does not always work, which is
+                // why the one above is used in most cases: the getter for a property parameter's
+                // source element will be a KtParameter, and the getter for a simple property
+                // declaration with no custom getter declaration will be a KtProperty, not a
+                // KtPropertyAccessor.
+                ?: allAccessors.singleOrNull {
+                        (it.psiMethod.sourceElement as? KtPropertyAccessor)?.isGetter == true
+                    }
+            }
+        }
+
+        /** Like [findGetter], but finds the property setter instead. */
+        private fun findSetter(
+            allAccessors: List<PsiMethodItem>,
+            propertyReceiverType: PsiTypeItem?
+        ): PsiMethodItem? {
+            return if (propertyReceiverType == null) {
+                // No receiver, the setter has one parameter.
+                allAccessors.singleOrNull { it.parameters().size == 1 }
+            } else {
+                // The setter has a receiver parameter in addition to the normal setter parameter.
+                allAccessors.singleOrNull {
+                    it.parameters().size == 2 && it.parameters()[0].type() == propertyReceiverType
+                }
+                // Work around a psi bug, see the equivalent [findGetter] case for details.
+                ?: allAccessors.singleOrNull {
+                        (it.psiMethod.sourceElement as? KtPropertyAccessor)?.isSetter == true
+                    }
+            }
         }
     }
 }
