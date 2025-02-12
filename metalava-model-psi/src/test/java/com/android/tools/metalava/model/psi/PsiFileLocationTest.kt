38f97ab7fc0c67c34431855df491be604811a762
Julia McClellan <juliamcclellan@google.com>
2025-02-12 16:41:53 -0500

    Fix tests which used multiple input files of the same format not in one input set
    Currently, if multiple inputs to a codebase test have the same format, the test passes without running the test lambda on any of the inputs. The followup CL changes that, which revealed that these tests were previously set up incorrectly.
    
    Bug: 377733789
    Test: ./gradlew test
    Change-Id: I12925ab451edd6a264d7b8bbc752569fbf5f27df

--- metalava-model-psi/src/test/java/com/android/tools/metalava/model/psi/PsiFileLocationTest.kt
+++ metalava-model-psi/src/test/java/com/android/tools/metalava/model/psi/PsiFileLocationTest.kt
@@ -27,8 +27,8 @@ class PsiFileLocationTest : BaseModelTest() {
      * Casts [item] to a [PsiItem] and gets the source psi of its underlying PsiElement. Generates a
      * baseline key, checking that the element ID of that key matches [expectedKey].
      */
-    private fun checkBaselineKeyFromSourcePsi(item: Item, expectedKey: String) {
-        val psi = (item as PsiItem).sourcePsi
+    private fun checkBaselineKeyFromPsi(item: Item, expectedKey: String) {
+        val psi = (item as PsiItem).psi()
         val baselineKey = PsiFileLocation.getBaselineKey(psi)
         assertEquals(expectedKey, baselineKey.elementId())
     }
@@ -36,24 +36,28 @@ class PsiFileLocationTest : BaseModelTest() {
     @Test
     fun `Baseline key for top level KtProperty`() {
         runCodebaseTest(
-            kotlin(
-                """
-                    package test.pkg
-                    val propertyInTestKt = 0
-                """
-            ),
-            kotlin(
-                """
-                    @file:JvmName("Foo")
-                    package test.pkg
-                    val propertyInFoo = 0
-                """
+            inputSet(
+                kotlin(
+                    "src/test/pkg/Test.kt",
+                    """
+                        package test.pkg
+                        val propertyInTestKt = 0
+                    """
+                ),
+                kotlin(
+                    "src/test/pkg/Foo.kt",
+                    """
+                        @file:JvmName("Foo")
+                        package test.pkg
+                        val propertyInFoo = 0
+                    """
+                )
             )
         ) {
             val testKtPropertyItem = codebase.assertClass("test.pkg.TestKt").properties().single()
-            checkBaselineKeyFromSourcePsi(testKtPropertyItem, "test.pkg.TestKt#propertyInTestKt")
+            checkBaselineKeyFromPsi(testKtPropertyItem, "test.pkg.TestKt#propertyInTestKt")
             val fooPropertyItem = codebase.assertClass("test.pkg.Foo").properties().single()
-            checkBaselineKeyFromSourcePsi(fooPropertyItem, "test.pkg.Foo#propertyInFoo")
+            checkBaselineKeyFromPsi(fooPropertyItem, "test.pkg.Foo#propertyInFoo")
         }
     }
 
@@ -70,7 +74,7 @@ class PsiFileLocationTest : BaseModelTest() {
             )
         ) {
             val foo = codebase.assertClass("test.pkg.Foo").methods().single()
-            checkBaselineKeyFromSourcePsi(
+            checkBaselineKeyFromPsi(
                 foo,
                 "test.pkg.Foo#foo(java.lang.String, java.util.List<? extends T>)"
             )
