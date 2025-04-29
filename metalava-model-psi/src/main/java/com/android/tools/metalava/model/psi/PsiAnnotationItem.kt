0605e14f50fc930976d190f42ed939315b38fd52
Paul Duffin <paulduffin@google.com>
2025-04-29 21:46:04 +0100

    Use default AnnotationItem.toSource() for PsiAnnotationItem
    Previously, `PsiAnnotationItem` had its own implementation of
    `AnnotationItem.toSource()`. This change removes that so it will use
    the default implementation.
    
    Bug: 354633349
    Test: ./gradlew
          # Tested on Android and AndroidX
    Change-Id: I37769e453c484621d5e0b28d621c6634c778cf05

--- metalava-model-psi/src/main/java/com/android/tools/metalava/model/psi/PsiAnnotationItem.kt
+++ metalava-model-psi/src/main/java/com/android/tools/metalava/model/psi/PsiAnnotationItem.kt
@@ -19,24 +19,10 @@ package com.android.tools.metalava.model.psi
 import com.android.tools.metalava.model.ANNOTATION_ATTR_VALUE
 import com.android.tools.metalava.model.AnnotationAttribute
 import com.android.tools.metalava.model.AnnotationItem
-import com.android.tools.metalava.model.AnnotationTarget
 import com.android.tools.metalava.model.Codebase
 import com.android.tools.metalava.model.DefaultAnnotationAttribute
 import com.android.tools.metalava.model.DefaultAnnotationItem
-import com.android.tools.metalava.model.Item
-import com.android.tools.metalava.model.psi.CodePrinter.Companion.constantToExpression
-import com.android.tools.metalava.model.psi.CodePrinter.Companion.constantToSource
-import com.intellij.psi.JavaTokenType
 import com.intellij.psi.PsiAnnotation
-import com.intellij.psi.PsiAnnotationMemberValue
-import com.intellij.psi.PsiArrayInitializerMemberValue
-import com.intellij.psi.PsiBinaryExpression
-import com.intellij.psi.PsiClass
-import com.intellij.psi.PsiExpression
-import com.intellij.psi.PsiField
-import com.intellij.psi.PsiLiteral
-import com.intellij.psi.PsiReference
-import com.intellij.psi.impl.JavaConstantExpressionEvaluator
 
 internal class PsiAnnotationItem
 private constructor(
@@ -52,18 +38,6 @@ private constructor(
         qualifiedName = qualifiedName,
         attributesGetter = { getAnnotationAttributes(annotationContext, psiAnnotation) },
     ) {
-
-    override fun toSource(target: AnnotationTarget, context: Item?): String {
-        val sb = StringBuilder(60)
-        appendAnnotation(
-            annotationContext,
-            sb,
-            psiAnnotation,
-            qualifiedName,
-            target,
-        )
-        return sb.toString()
-    }
 
     override fun snapshot(targetCodebase: Codebase) = this
 
@@ -110,185 +84,6 @@ private constructor(
                 originalName = originalName,
                 qualifiedName = qualifiedName,
             )
-        }
-
-        private fun appendAnnotation(
-            codebase: PsiBasedCodebase,
-            sb: StringBuilder,
-            psiAnnotation: PsiAnnotation,
-            qualifiedName: String?,
-            target: AnnotationTarget,
-        ) {
-            qualifiedName ?: return
-            val outputName = codebase.annotationManager.normalizeOutputName(qualifiedName, target)
-
-            val alwaysInlineValues = qualifiedName == "android.annotation.FlaggedApi"
-            val attributes = psiAnnotation.parameterList.attributes
-            if (attributes.isEmpty()) {
-                sb.append("@$outputName")
-                return
-            }
-
-            sb.append("@")
-            sb.append(outputName)
-            sb.append("(")
-            if (
-                attributes.size == 1 &&
-                    (attributes[0].name == null || attributes[0].name == ANNOTATION_ATTR_VALUE)
-            ) {
-                // Special case: omit "value" if it's the only attribute
-                appendValue(
-                    codebase,
-                    sb,
-                    attributes[0].value,
-                    target,
-                    alwaysInlineValues = alwaysInlineValues,
-                )
-            } else {
-                var first = true
-                for (attribute in attributes) {
-                    if (first) {
-                        first = false
-                    } else {
-                        sb.append(", ")
-                    }
-                    sb.append(attribute.name ?: ANNOTATION_ATTR_VALUE)
-                    sb.append('=')
-                    appendValue(
-                        codebase,
-                        sb,
-                        attribute.value,
-                        target,
-                        alwaysInlineValues = alwaysInlineValues,
-                    )
-                }
-            }
-            sb.append(")")
-        }
-
-        private fun appendValue(
-            codebase: PsiBasedCodebase,
-            sb: StringBuilder,
-            value: PsiAnnotationMemberValue?,
-            target: AnnotationTarget,
-            alwaysInlineValues: Boolean,
-        ) {
-            // Compute annotation string -- we don't just use value.text here
-            // because that may not use fully qualified names, e.g. the source may say
-            //  @RequiresPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
-            // and we want to compute
-            //
-            // @androidx.annotation.RequiresPermission(android.Manifest.permission.ACCESS_COARSE_LOCATION)
-            when (value) {
-                null -> sb.append("null")
-                is PsiLiteral -> sb.append(constantToSource(value.value))
-                is PsiReference -> {
-                    when (val resolved = value.resolve()) {
-                        is PsiField -> {
-                            val containing = resolved.containingClass
-                            if (containing != null) {
-                                // If it's a field reference, see if it looks like the field is
-                                // hidden; if
-                                // so, inline the value
-                                val cls = codebase.findOrCreateClass(containing)
-                                val initializer = resolved.initializer
-                                if (initializer != null) {
-                                    val fieldItem = cls.findField(resolved.name)
-                                    if (
-                                        alwaysInlineValues ||
-                                            fieldItem == null ||
-                                            fieldItem.isHiddenOrRemoved() ||
-                                            !fieldItem.isPublic
-                                    ) {
-                                        // Use the literal value instead
-                                        val source = getConstantSource(initializer)
-                                        if (source != null) {
-                                            sb.append(source)
-                                            return
-                                        }
-                                    }
-                                }
-                                containing.qualifiedName?.let { sb.append(it).append('.') }
-                            }
-
-                            sb.append(resolved.name)
-                        }
-                        is PsiClass -> resolved.qualifiedName?.let { sb.append(it) }
-                        else -> {
-                            sb.append(value.text)
-                        }
-                    }
-                }
-                is PsiArrayInitializerMemberValue -> {
-                    sb.append('{')
-                    var first = true
-                    for (initializer in value.initializers) {
-                        if (first) {
-                            first = false
-                        } else {
-                            sb.append(", ")
-                        }
-                        appendValue(
-                            codebase,
-                            sb,
-                            initializer,
-                            target,
-                            alwaysInlineValues = alwaysInlineValues,
-                        )
-                    }
-                    sb.append('}')
-                }
-                is PsiAnnotation -> {
-                    value.qualifiedName?.let { qualifiedName ->
-                        appendAnnotation(
-                            codebase,
-                            sb,
-                            value,
-                            // Normalize the input name of the annotation.
-                            codebase.annotationManager.normalizeInputName(qualifiedName),
-                            target,
-                        )
-                    }
-                }
-                else -> {
-                    // Special case the formatting of special floating point numbers which are
-                    // defined in terms of division by 0.
-                    if (
-                        value is PsiBinaryExpression &&
-                            value.operationTokenType == JavaTokenType.DIV
-                    ) {
-                        val right = value.rOperand
-                        if (right is PsiLiteral) {
-                            if (right.value == 0.0f || right.value == 0.0) {
-                                val left = value.lOperand
-                                if (left is PsiLiteral) {
-                                    sb.append(constantToSource(left.value))
-                                } else {
-                                    val source = getConstantSource(left)
-                                    sb.append(source)
-                                }
-                                sb.append(" / ")
-                                sb.append(constantToSource(right.value))
-                                return
-                            }
-                        }
-                    }
-
-                    if (value is PsiExpression) {
-                        val source = getConstantSource(value)
-                        if (source != null) {
-                            sb.append(source)
-                            return
-                        }
-                    }
-                    sb.append(value.text)
-                }
-            }
-        }
-
-        private fun getConstantSource(value: PsiExpression): String? {
-            val constant = JavaConstantExpressionEvaluator.computeConstantExpression(value, false)
-            return constantToExpression(constant)
         }
     }
 }
