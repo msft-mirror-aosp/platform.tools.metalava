4aa162cd4b2d19d8aea32edbd809c7f5d69b234b
Paul Duffin <paulduffin@google.com>
2025-06-09 19:26:00 +0100

    Sort annotation attributes in stub files
    Previously, stub files would output annotation attributes in the order
    in which they were specified in the source file. That made the stub
    files dependent on changes to the source files that did not affect the
    API. This change causes it to sort the attributes before formatting
    avoiding that dependency.
    
    Bug: 354633349
    Test: ./gradlew
    Change-Id: Id85bd8d823a0b91f2a828503f0e7546e0481b5ba

--- metalava/src/test/java/com/android/tools/metalava/doc/DocAnalyzerTest.kt
+++ metalava/src/test/java/com/android/tools/metalava/doc/DocAnalyzerTest.kt
@@ -1795,7 +1795,7 @@ class DocAnalyzerTest : DriverTest() {
                     /**
                      * This constant represents a column name that can be used with a {@link android.content.ContentProvider} through a {@link android.content.ContentValues} or {@link android.database.Cursor} object. The values stored in this column are {@link Cursor.NONEXISTENT}, and are read-only and cannot be mutated.
                      */
-                    @android.provider.Column(value=Cursor.NONEXISTENT, readOnly=true) public static final java.lang.String BOGUS = "bogus";
+                    @android.provider.Column(readOnly=true, value=Cursor.NONEXISTENT) public static final java.lang.String BOGUS = "bogus";
                     /**
                      * This constant represents a column name that can be used with a {@link android.content.ContentProvider} through a {@link android.content.ContentValues} or {@link android.database.Cursor} object. The values stored in this column are {@link android.database.Cursor#FIELD_TYPE_STRING Cursor#FIELD_TYPE_STRING} .
                      */
@@ -1803,11 +1803,11 @@ class DocAnalyzerTest : DriverTest() {
                     /**
                      * This constant represents a column name that can be used with a {@link android.content.ContentProvider} through a {@link android.content.ContentValues} or {@link android.database.Cursor} object. The values stored in this column are {@link android.database.Cursor#FIELD_TYPE_BLOB Cursor#FIELD_TYPE_BLOB} , and are read-only and cannot be mutated.
                      */
-                    @android.provider.Column(value=android.database.Cursor.FIELD_TYPE_BLOB, readOnly=true) public static final java.lang.String HASH = "_hash";
+                    @android.provider.Column(readOnly=true, value=android.database.Cursor.FIELD_TYPE_BLOB) public static final java.lang.String HASH = "_hash";
                     /**
                      * This constant represents a column name that can be used with a {@link android.content.ContentProvider} through a {@link android.content.ContentValues} or {@link android.database.Cursor} object. The values stored in this column are {@link android.database.Cursor#FIELD_TYPE_STRING Cursor#FIELD_TYPE_STRING} , and are read-only and cannot be mutated.
                      */
-                    @android.provider.Column(value=android.database.Cursor.FIELD_TYPE_STRING, readOnly=true) public static final java.lang.String TITLE = "title";
+                    @android.provider.Column(readOnly=true, value=android.database.Cursor.FIELD_TYPE_STRING) public static final java.lang.String TITLE = "title";
                     }
                     """
                     )
