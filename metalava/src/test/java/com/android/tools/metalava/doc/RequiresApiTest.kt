a9711573f8849668fd0510deee59b92c35965b89
Paul Duffin <paulduffin@google.com>
2025-08-14 14:27:14 +0100

    Extract RequiresApi tests from DocAnalyzerTest
    Preparation for making some changes to `RequiresApi` handling.
    
    Bug: 424435764
    Test: ./gradlew
    Change-Id: Id69c4fd4ad425c21dc75727f22fae607292f86fb

---
+++ metalava/src/test/java/com/android/tools/metalava/doc/RequiresApiTest.kt
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.tools.metalava.doc
+
+import com.android.tools.metalava.DriverTest
+import com.android.tools.metalava.requiresApiSource
+import com.android.tools.metalava.testing.java
+import org.junit.Test
+
+/** Test handling of `RequiresApi` annotation in [DocAnalyzer]. */
+class RequiresApiTest : DriverTest() {
+    @Test
+    fun `Check RequiresApi handling`() {
+        check(
+            sourceFiles =
+                arrayOf(
+                    java(
+                        """
+                            package test.pkg;
+                            import androidx.annotation.RequiresApi;
+                            @RequiresApi(value = 21)
+                            public class MyClass1 {
+                            }
+                        """
+                    ),
+                    requiresApiSource
+                ),
+            docStubs = true,
+            stubFiles =
+                arrayOf(
+                    java(
+                        """
+                            package test.pkg;
+                            /** @apiSince 21 */
+                            @SuppressWarnings({"unchecked", "deprecation", "all"})
+                            public class MyClass1 {
+                            public MyClass1() { throw new RuntimeException("Stub!"); }
+                            }
+                        """
+                    )
+                )
+        )
+    }
+}
