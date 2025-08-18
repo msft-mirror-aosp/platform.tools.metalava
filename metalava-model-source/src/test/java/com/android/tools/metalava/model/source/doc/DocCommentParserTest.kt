269474670725aafc386123bde8a2d0e1b88b3cd2
Paul Duffin <paulduffin@google.com>
2025-08-18 18:04:26 +0100

    Add DocComment model and parser
    The first use that is made of the Javadoc/KDoc comment in Metalava is
    to search for `@hide`, `@doconly` and `@removed` doc tags to set the
    `isHidden`, `isDocOnly` and `isRemoved` properties in
    `ItemDocumentation`. Those checks are performed on most classes and
    class members in the source, not just the ones that are part of the API
    surface, so they have to be quick but fortunately, they only require
    quite simple parsing of the comment.
    
    This change adds `DocComment` model, a `DocCommentParser` for
    constructing a `DocComment` from some text and some tests to show
    that it behaves correctly. This initial implementation supports the
    standard Javadoc/KDoc syntax. A follow-up change will customize it to
    handle some Android special cases.
    
    Bug: 429965593
    Test: ./gradlew
    Change-Id: I3eae984041f290c2929ba1244075b941e7530a1d

---
+++ metalava-model-source/src/test/java/com/android/tools/metalava/model/source/doc/DocCommentParserTest.kt
@@ -0,0 +1,174 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.tools.metalava.model.source.doc
+
+import kotlin.test.assertEquals
+import org.junit.Test
+
+class DocCommentParserTest {
+    /** Create a [DocComment] from [input], compare it against the [expectedString] */
+    private fun checkDocComment(input: String, expectedString: String) {
+        var docComment = DocCommentParser.parseText(input.trimIndent())
+        assertEquals(expectedString.trimIndent(), docComment.toString())
+    }
+
+    @Test
+    fun `Test empty comment`() {
+        checkDocComment(
+            input = "",
+            expectedString = "description: <<>>",
+        )
+    }
+
+    @Test
+    fun `Test description`() {
+        checkDocComment(
+            input = "Description",
+            expectedString = "description: <<Description>>",
+        )
+    }
+
+    @Test
+    fun `Test description with nested braces`() {
+        checkDocComment(
+            input = "Description {@code something}",
+            expectedString = "description: <<Description {@code something}>>",
+        )
+    }
+
+    @Test
+    fun `Test no description block tag`() {
+        checkDocComment(
+            input = "@see something",
+            expectedString =
+                """
+                    description: <<>>
+                    @see <<something>>
+                """,
+        )
+    }
+
+    @Test
+    fun `Test description and block tags without javadoc comment`() {
+        checkDocComment(
+            input =
+                """
+                    Some text
+                    @see something
+                    @see other thing
+                """,
+            expectedString =
+                """
+                    description: <<Some text>>
+                    @see <<something>>
+                    @see <<other thing>>
+                """,
+        )
+    }
+
+    @Test
+    fun `Test description and block tags in javadoc comment`() {
+        checkDocComment(
+            input =
+                """
+                    /**
+                     * Some text
+                     * @see something
+                     * @see other thing
+                     */
+                """,
+            expectedString =
+                """
+                    description: <<\n * Some text>>
+                    @see <<something>>
+                    @see <<other thing>>
+                """,
+        )
+    }
+
+    @Test
+    fun `Test block tag in single line javadoc comment`() {
+        checkDocComment(
+            input =
+                """
+                    /** @hide */
+                """,
+            expectedString =
+                """
+                    description: <<>>
+                    @hide <<>>
+                """,
+        )
+    }
+
+    @Test
+    fun `Test multiple block tags in javadoc comment`() {
+        checkDocComment(
+            input =
+                """
+                    /**
+                     * @hide
+                     * @deprecated
+                     */
+                """,
+            expectedString =
+                """
+                    description: <<>>
+                    @hide <<>>
+                    @deprecated <<>>
+                """,
+        )
+    }
+
+    @Test
+    fun `Test a block @hide tag and some text`() {
+        checkDocComment(
+            input =
+                """
+                    /**
+                     * A block @hide tag.
+                     *
+                     * @hide
+                     */
+                """,
+            expectedString =
+                """
+                    description: <<\n * A block @hide tag.\n *>>
+                    @hide <<>>
+                """,
+        )
+    }
+
+    @Test
+    fun `Test an unbalanced open brace`() {
+        checkDocComment(
+            input =
+                """
+                    /**
+                     * An unbalanced open {
+                     *
+                     * @hide
+                     */
+                """,
+            expectedString =
+                """
+                    description: <<\n * An unbalanced open {\n *>>
+                    @hide <<>>
+                """,
+        )
+    }
+}
