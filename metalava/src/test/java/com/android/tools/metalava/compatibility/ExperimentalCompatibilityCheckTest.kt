282dfd4d8889c293ffb0389ef02eed741b797997
Aidan Melvin <aidmelvin@google.com>
2025-11-03 17:01:11 +0000

    Added test for changed default on experimental fun
    Currently, if a method is marked experimental within an interface
    and it is changed from default to abstract, this will break
    clients that implemented the interface but didn't implement the method.
    This commit adds a test to demonstrate the issue, the followup commit
    will add the actual compatibility check.
    
    Bug: 454031123
    Test: N/A
    Change-Id: I90fb62a7d1ca7398f162c9e4ba4b1ca0cdde3884

--- metalava/src/test/java/com/android/tools/metalava/compatibility/ExperimentalCompatibilityCheckTest.kt
+++ metalava/src/test/java/com/android/tools/metalava/compatibility/ExperimentalCompatibilityCheckTest.kt
@@ -24,6 +24,39 @@ import org.junit.Test
 
 class ExperimentalCompatibilityCheckTest : DriverTest() {
 
+    /**
+     * TODO: change this to "Should raise compatibility error on changed default on method in interface"
+     */
+    @Test
+    fun `Don't raise compatibility error on changed default on method in interface`() {
+        check(
+            expectedIssues =
+                """
+                """,
+            checkCompatibilityApiReleased =
+                """
+package test.pkg {
+  @java.lang.annotation.Retention(java.lang.annotation.RetentionPolicy.RUNTIME) public @interface ExperimentalAnnotation {
+  }
+  public interface MyInterface {
+    method @test.pkg.ExperimentalAnnotation public default void myFun();
+  }
+}
+                """,
+            signatureSource =
+                """
+package test.pkg {
+  @java.lang.annotation.Retention(java.lang.annotation.RetentionPolicy.RUNTIME) public @interface ExperimentalAnnotation {
+  }
+  public interface MyInterface {
+    method @test.pkg.ExperimentalAnnotation public void myFun();
+  }
+}
+                """,
+            suppressCompatibilityMetaAnnotations = arrayOf("test.pkg.ExperimentalAnnotation")
+        )
+    }
+
     @Test
     fun `Should raise compatibility error on changed method return type in abstract class or interface`() {
         check(
