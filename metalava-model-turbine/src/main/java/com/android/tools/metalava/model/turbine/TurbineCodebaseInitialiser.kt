3c9a608fd2617fbf6ad09154a821209efdb46ae9
Paul Duffin <paulduffin@google.com>
2025-11-19 11:48:07 +0000

    Stop Turbine extracting package-info.java in gatherPackageJavadoc(...)
    Previously, Turbine used `gatherPackageJavadoc(...)` to extract
    information from `package-info.java` files. However, that is no longer
    necessary as the preceding change now extracts that directly from the
    underlying model.
    
    This change stops Turbine extracting that information using
    `gatherPackageJavadoc(...)` and cleans up the calling code.
    
    Bug: 461491536
    Test: ./gradlew
    Change-Id: I21e7c8b95f68d29d5662bb177ff639bd6466e6a0

--- metalava-model-turbine/src/main/java/com/android/tools/metalava/model/turbine/TurbineCodebaseInitialiser.kt
+++ metalava-model-turbine/src/main/java/com/android/tools/metalava/model/turbine/TurbineCodebaseInitialiser.kt
@@ -21,7 +21,6 @@ import com.android.tools.metalava.model.ApiVariantSelectors
 import com.android.tools.metalava.model.ClassItem
 import com.android.tools.metalava.model.ClassOrigin
 import com.android.tools.metalava.model.Item
-import com.android.tools.metalava.model.JAVA_PACKAGE_INFO
 import com.android.tools.metalava.model.PackageFilter
 import com.android.tools.metalava.model.PackageItem
 import com.android.tools.metalava.model.SourceLanguage
@@ -29,8 +28,6 @@ import com.android.tools.metalava.model.TypeParameterScope
 import com.android.tools.metalava.model.item.DefaultCodebaseAssembler
 import com.android.tools.metalava.model.item.DefaultCodebaseFactory
 import com.android.tools.metalava.model.item.DefaultItemFactory
-import com.android.tools.metalava.model.item.DefaultPackageItem
-import com.android.tools.metalava.model.item.MutablePackageDoc
 import com.android.tools.metalava.model.item.PackageDocs
 import com.android.tools.metalava.model.item.PackageInfo
 import com.android.tools.metalava.model.source.NO_SOURCE_COMMENT_FACTORY
@@ -255,43 +252,14 @@ internal class TurbineCodebaseInitialiser(
         globalTypeItemFactory =
             TurbineTypeItemFactory(this, annotationFactory, TypeParameterScope.empty)
 
-        // Find the package-info.java units.
-        val packageInfoUnits = allUnits.filter { it.isPackageInfo() }
-
-        // Split the map from ClassSymbol to SourceTypeBoundClass into separate package-info and
-        // normal classes.
-        val (packageInfoClasses, allSourceClasses) =
-            separatePackageInfoClassesFromRealClasses(allSourceClassMap)
-
-        val packageInfoList =
-            combinePackageInfoClassesAndUnits(packageInfoClasses, packageInfoUnits)
-
-        // Scan the files looking for package.html and overview.html files and combine that with
-        // information from package-info.java units to create a comprehensive set of package
+        // Scan the files looking for package.html and overview.html files and extract the
         // documentation just in case they are needed during package creation.
-        val packageDocs =
-            gatherPackageJavadoc(
-                codebase.reporter,
-                sourceSet,
-                packageNameFilter = { isValidPackage(it) },
-                packageInfoList
-            ) { (unit, packageName) ->
-                val file = File(unit.source().path())
-                val fileLocation = FileLocation.forFile(file)
-                val turbineSourceFile = sourceFileCache.turbineSourceFile(unit.source())
-                val comment = itemDocumentationFactoryForDecl(turbineSourceFile, unit.pkg().get())
-
-                MutablePackageDoc(
-                    qualifiedName = packageName,
-                    fileLocation = fileLocation,
-                    commentFactory = comment,
-                )
-            }
+        val packageDocs = gatherPackageJavadoc(sourceSet) { isValidPackage(it) }
 
         // Get the map from ClassSymbol to SourceTypeBoundClass for only those classes provided on
         // the command line as only those classes can contribute directly to the API.
         val commandLineSourceClasses =
-            topLevelAccessibleCommandLineClasses(allSourceClasses, commandLineSources)
+            topLevelAccessibleCommandLineClasses(allSourceClassMap, commandLineSources)
 
         createAllPackages(packageDocs)
         createAllCommandLineClasses(commandLineSourceClasses, apiPackages)
@@ -343,7 +311,7 @@ internal class TurbineCodebaseInitialiser(
      *
      * @param allSourceClasses all the [SourceTypeBoundClass]s found during binding, includes those
      *   from the source path as well as those whose containing file was provided on the command
-     *   line.
+     *   line. Also, includes `package-info.java` classes.
      * @param commandLineSources the list of source [File]s provided on the command line.
      */
     private fun topLevelAccessibleCommandLineClasses(
@@ -356,7 +324,10 @@ internal class TurbineCodebaseInitialiser(
         // Get the map from ClassSymbol to SourceTypeBoundClass for only the accessible, top level
         // classes provided on the command line as only those classes (and their nested classes) can
         // contribute directly to the API.
-        return allSourceClasses.filter { (_, sourceTypeBoundClass) ->
+        return allSourceClasses.filter { (symbol, sourceTypeBoundClass) ->
+            // Ignore all `package-info.java` classes.
+            if (symbol.simpleName() == "package-info") return@filter false
+
             // Ignore nested classes, they will be created as part of the construction of their
             // containing class.
             if (sourceTypeBoundClass.owner() != null) return@filter false
@@ -415,31 +386,6 @@ internal class TurbineCodebaseInitialiser(
     override fun typeBoundClassForSymbol(classSymbol: ClassSymbol) = envClassMap.get(classSymbol)!!
 
     /**
-     * Separate `package-info.java` synthetic classes from real classes.
-     *
-     * Turbine treats a `package-info.java` file as if it created a class called `package-info`.
-     * This method separates the [sourceClassMap] into two, one for the synthetic `package-info`
-     * classes and one for real classes.
-     *
-     * @param sourceClassMap the map from [ClassSymbol] to [SourceTypeBoundClass] for all classes,
-     *   real or synthetic.
-     */
-    private fun separatePackageInfoClassesFromRealClasses(
-        sourceClassMap: Map<ClassSymbol, SourceTypeBoundClass>,
-    ): Pair<Map<ClassSymbol, SourceTypeBoundClass>, Map<ClassSymbol, SourceTypeBoundClass>> {
-        val packageInfoClasses = mutableMapOf<ClassSymbol, SourceTypeBoundClass>()
-        val sourceClasses = mutableMapOf<ClassSymbol, SourceTypeBoundClass>()
-        for ((symbol, typeBoundClass) in sourceClassMap) {
-            if (symbol.simpleName() == "package-info") {
-                packageInfoClasses[symbol] = typeBoundClass
-            } else {
-                sourceClasses[symbol] = typeBoundClass
-            }
-        }
-        return Pair(packageInfoClasses, sourceClasses)
-    }
-
-    /**
      * Convert this qualified name consisting of a list of identifiers separated by '.' into a list
      * of identifiers.
      *
@@ -453,35 +399,6 @@ internal class TurbineCodebaseInitialiser(
      */
     private fun isValidPackage(packageName: String) =
         index.lookupPackage(packageName.qualifiedNameToIdentifierList()) != null
-
-    /**
-     * Encapsulates information needed to create a [DefaultPackageItem] in [gatherPackageJavadoc].
-     */
-    data class PackageInfoClass(
-        val unit: CompUnit,
-        val packageName: String,
-    )
-
-    /** Combine `package-info.java` synthetic classes and units */
-    private fun combinePackageInfoClassesAndUnits(
-        sourceClassMap: Map<ClassSymbol, SourceTypeBoundClass>,
-        packageInfoUnits: List<CompUnit>
-    ): List<PackageInfoClass> {
-        // Create a mapping between the package name and the unit.
-        val packageInfoMap = packageInfoUnits.associateBy { getPackageName(it) }
-
-        return sourceClassMap.keys.map { symbol ->
-            val packageName = symbol.packageName().replace('/', '.')
-            PackageInfoClass(
-                unit = packageInfoMap[packageName]!!,
-                packageName = packageName,
-            )
-        }
-    }
-
-    /** Check if this is for a `package-info.java` file or not. */
-    private fun CompUnit.isPackageInfo() =
-        source().path().let { it == JAVA_PACKAGE_INFO || it.endsWith("/" + JAVA_PACKAGE_INFO) }
 
     private fun createAllPackages(packageDocs: PackageDocs) {
         // Create packages for all the documentation packages and make sure there is a root package.
