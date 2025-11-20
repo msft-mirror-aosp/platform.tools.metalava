1b68d2ba296549e6f8b91d744334fb1e821dc434
Paul Duffin <paulduffin@google.com>
2025-11-20 18:39:34 +0000

    Differentiate between optional and require documentation in tests
    Follows-on from previous change.
    
    Bug: 462419767
    Test: ./gradlew
    Change-Id: I01cbb5bbf3f6275eed2e90db4c99842f7a6cf37d

--- metalava-model-testsuite/src/main/java/com/android/tools/metalava/model/testsuite/documentation/CommonItemDocumentationTest.kt
+++ metalava-model-testsuite/src/main/java/com/android/tools/metalava/model/testsuite/documentation/CommonItemDocumentationTest.kt
@@ -248,7 +248,7 @@ class CommonItemDocumentationTest : BaseModelTest() {
         item: SelectableItem,
         expectedLocation: String
     ) {
-        val documentation = item.documentation
+        val documentation = item.requiredDocumentation
         val location = documentation.fileLocation
         assertEquals(expectedLocation, removeTestSpecificDirectories(location.toString()))
     }
@@ -507,7 +507,7 @@ class CommonItemDocumentationTest : BaseModelTest() {
             testClass.assertMethod("noCommentAppendToMainDescription", emptyList()).let { methodItem
                 ->
                 // Add to main description first.
-                methodItem.documentation.mainDescriptionOwner.append("Appended to main.")
+                methodItem.requiredDocumentation.mainDescriptionOwner.append("Appended to main.")
                 methodItem.assertPrintedDocumentation(
                     expectedOutput =
                         """
@@ -520,7 +520,7 @@ class CommonItemDocumentationTest : BaseModelTest() {
                 )
 
                 // Add to deprecated second.
-                methodItem.documentation
+                methodItem.requiredDocumentation
                     .blockTagDescriptionOwner("deprecated")
                     .append("Appended to deprecated.")
                 methodItem.assertPrintedDocumentation(
@@ -538,7 +538,7 @@ class CommonItemDocumentationTest : BaseModelTest() {
 
             testClass.assertMethod("noCommentAppendDeprecated", emptyList()).let { methodItem ->
                 // Add to deprecated first.
-                methodItem.documentation
+                methodItem.requiredDocumentation
                     .blockTagDescriptionOwner("deprecated")
                     .append("Appended to deprecated.")
                 methodItem.assertPrintedDocumentation(
@@ -558,7 +558,7 @@ class CommonItemDocumentationTest : BaseModelTest() {
                 //  the main description is added first.
 
                 // Add to main second.
-                methodItem.documentation.mainDescriptionOwner.append("Appended to main.")
+                methodItem.requiredDocumentation.mainDescriptionOwner.append("Appended to main.")
                 methodItem.assertPrintedDocumentation(
                     expectedOutput =
                         """
@@ -662,7 +662,7 @@ class CommonItemDocumentationTest : BaseModelTest() {
                     """,
             )
 
-            testClass.documentation.addUniqueBlockTagSectionWithSimpleText("unique", "1")
+            testClass.requiredDocumentation.addUniqueBlockTagSectionWithSimpleText("unique", "1")
 
             testClass.assertPrintedDocumentation(
                 expectedOutput =
@@ -674,7 +674,7 @@ class CommonItemDocumentationTest : BaseModelTest() {
                     """,
             )
 
-            testClass.documentation.addUniqueBlockTagSectionWithSimpleText("unique", "2")
+            testClass.requiredDocumentation.addUniqueBlockTagSectionWithSimpleText("unique", "2")
 
             testClass.assertPrintedDocumentation(
                 expectedOutput =
@@ -936,7 +936,7 @@ class CommonItemDocumentationTest : BaseModelTest() {
             ),
         ) {
             val testClass = codebase.assertClass("test.pkg.Test")
-            val documentation = testClass.documentation
+            val documentation = testClass.requiredDocumentation
 
             assertDocContentToString(
                 documentation.mainDescription,
@@ -959,7 +959,7 @@ class CommonItemDocumentationTest : BaseModelTest() {
             ),
         ) {
             val testClass = codebase.assertClass("test.pkg.Test")
-            val documentation = testClass.documentation
+            val documentation = testClass.requiredDocumentation
 
             assertDocContentToString(
                 documentation.mainDescription,
@@ -993,7 +993,7 @@ class CommonItemDocumentationTest : BaseModelTest() {
         ) {
             val testClass = codebase.assertClass("test.pkg.Test")
             val testMethod = testClass.methods().single()
-            val documentation = testMethod.documentation
+            val documentation = testMethod.requiredDocumentation
 
             assertDocContentToString(
                 documentation.paramTagDescription("p1"),
@@ -1046,10 +1046,13 @@ class CommonItemDocumentationTest : BaseModelTest() {
         ) {
             val otherClass = codebase.assertClass("test.other.Other")
             val contentToAppend =
-                otherClass.documentation.blockTagDescription("memberDoc", forAppending = true)!!
+                otherClass.requiredDocumentation.blockTagDescription(
+                    "memberDoc",
+                    forAppending = true
+                )!!
 
             val testClass = codebase.assertClass("test.pkg.Test")
-            val classDocumentation = testClass.documentation
+            val classDocumentation = testClass.requiredDocumentation
 
             testClass.assertPrintedDocumentation(expectedOutput = "", message = "before mutation")
 
@@ -1094,7 +1097,7 @@ class CommonItemDocumentationTest : BaseModelTest() {
             val testClass = codebase.assertClass("test.pkg.Test")
 
             val testMethod = testClass.methods().single()
-            val methodDocumentation = testMethod.documentation
+            val methodDocumentation = testMethod.requiredDocumentation
 
             testMethod.assertPrintedDocumentation(expectedOutput = "", message = "before mutation")
 
@@ -1131,7 +1134,7 @@ class CommonItemDocumentationTest : BaseModelTest() {
             ),
         ) {
             val testClass = codebase.assertClass("test.pkg.Test")
-            val documentation = testClass.documentation
+            val documentation = testClass.requiredDocumentation
 
             testClass.assertDocumentationText(
                 expectedOutput = "/** @deprecated */",
@@ -1169,7 +1172,7 @@ class CommonItemDocumentationTest : BaseModelTest() {
             ),
         ) {
             val testClass = codebase.assertClass("test.pkg.Test")
-            val documentation = testClass.documentation
+            val documentation = testClass.requiredDocumentation
 
             testClass.assertDocumentationText(expectedOutput = "", message = "text before mutation")
 
@@ -1245,7 +1248,7 @@ class CommonItemDocumentationTest : BaseModelTest() {
             val testClass = codebase.assertClass("test.pkg.Test")
 
             val testMethod = testClass.methods().single()
-            val documentation = testMethod.documentation
+            val documentation = testMethod.requiredDocumentation
 
             testMethod.assertDocumentationText(
                 expectedOutput = "/** @param p */",
@@ -1285,7 +1288,7 @@ class CommonItemDocumentationTest : BaseModelTest() {
         ) {
             val testClass = codebase.assertClass("test.pkg.Test")
             val testMethod = testClass.methods().single()
-            val documentation = testMethod.documentation
+            val documentation = testMethod.requiredDocumentation
 
             testMethod.assertDocumentationText(
                 expectedOutput = "",
