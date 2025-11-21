8446bde328db4fc123c469e452fb7bdc9cd8da5c
Paul Duffin <paulduffin@google.com>
2025-11-21 11:45:35 +0000

    Simplify PsiBasedClassPathResolver
    Previously, it was duplicating functionality from `PsiBasedCodebase`.
    This change removes that and just delegates the work directly to the
    `PsiBasedCodebase.resolveClass(String)` method.
    
    Bug: 462658629
    Test: ./gradlew
    Change-Id: I068d95dbf1091737eb60d59edee7b7c46518113c

--- metalava-model-psi/src/main/java/com/android/tools/metalava/model/psi/PsiBasedClassPathResolver.kt
+++ metalava-model-psi/src/main/java/com/android/tools/metalava/model/psi/PsiBasedClassPathResolver.kt
@@ -17,12 +17,9 @@
 package com.android.tools.metalava.model.psi
 
 import com.android.tools.lint.UastEnvironment
-import com.android.tools.metalava.model.ClassItem
 import com.android.tools.metalava.model.ClassPathResolver
 import com.android.tools.metalava.model.Codebase
 import com.android.tools.metalava.model.source.SourceSet
-import com.intellij.psi.JavaPsiFacade
-import com.intellij.psi.search.GlobalSearchScope
 import java.io.File
 
 internal class PsiBasedClassPathResolver(
@@ -30,16 +27,9 @@ internal class PsiBasedClassPathResolver(
     config: Codebase.Config,
     allowReadingComments: Boolean,
 ) : ClassPathResolver {
-    private val javaPsiFacade: JavaPsiFacade
-    private val searchScope: GlobalSearchScope
     private val classpathCodebase: PsiBasedCodebase
 
     init {
-        // Properties used to resolve classes from the classpath
-        val project = uastEnvironment.ideaProject
-        javaPsiFacade = JavaPsiFacade.getInstance(project)
-        searchScope = GlobalSearchScope.everythingScope(project)
-
         val assembler =
             PsiCodebaseAssembler(uastEnvironment) { assembler ->
                 PsiBasedCodebase(
@@ -56,10 +46,5 @@ internal class PsiBasedClassPathResolver(
         classpathCodebase = assembler.codebase
     }
 
-    override fun resolveClass(erasedName: String): ClassItem? {
-        // If the class cannot be found on the class path then return null, otherwise create a
-        // PsiClassItem for it.
-        val psiClass = javaPsiFacade.findClass(erasedName, searchScope) ?: return null
-        return classpathCodebase.findOrCreateClass(psiClass)
-    }
+    override fun resolveClass(erasedName: String) = classpathCodebase.resolveClass(erasedName)
 }
