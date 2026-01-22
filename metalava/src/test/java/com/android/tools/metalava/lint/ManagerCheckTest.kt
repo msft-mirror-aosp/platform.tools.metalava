6259808a8dee473787694048878e48e945397f00
Paul Duffin <paulduffin@google.com>
2026-01-22 08:46:16 +0000

    Improve test coverage of Manager related tests
    Adds some more ways to incorrectly return a `Manager`. Returning an
    array also triggers the `ParcelableList` check.
    
    Bug: 477515664
    Test: ./gradlew
    Change-Id: Ic0d3b20ea4c2532024a37213b946503374724956

--- metalava/src/test/java/com/android/tools/metalava/lint/ManagerCheckTest.kt
+++ metalava/src/test/java/com/android/tools/metalava/lint/ManagerCheckTest.kt
@@ -17,7 +17,9 @@
 package com.android.tools.metalava.lint
 
 import com.android.tools.metalava.DriverTest
+import com.android.tools.metalava.androidxNonNullSource
 import com.android.tools.metalava.androidxNullableSource
+import com.android.tools.metalava.cli.common.ARG_HIDE
 import com.android.tools.metalava.testing.java
 import org.junit.Test
 
@@ -26,10 +28,15 @@ class ManagerCheckTest : DriverTest() {
     fun `Check Manager related issues`() {
         check(
             apiLint = "", // enabled
+            // Ignore other issues.
+            extraArguments = arrayOf(ARG_HIDE, "ArrayReturn"),
             expectedIssues =
                 """
-                    src/android/pkg/MyFirstManager.java:6: error: Managers must always be obtained from Context; no direct constructors [ManagerConstructor]
-                    src/android/pkg/MyFirstManager.java:9: error: Managers must always be obtained from Context (`get`) [ManagerLookup]
+                    src/android/pkg/MyFirstManager.java:8: error: Managers must always be obtained from Context; no direct constructors [ManagerConstructor]
+                    src/android/pkg/MyFirstManager.java:11: error: Managers must always be obtained from Context (`error1`) [ManagerLookup]
+                    src/android/pkg/MyFirstManager.java:13: warning: Methods should return `List<? extends Parcelable>` instead of `Parcelable[]` to support `ParceledListSlice` under the hood: method android.pkg.MyFirstManager.error2() [ParcelableList]
+                    src/android/pkg/MyFirstManager.java:13: error: Managers must always be obtained from Context (`error2`) [ManagerLookup]
+                    src/android/pkg/MySecondManager.java:8: error: Managers must always be obtained from Context (`error1`) [ManagerLookup]
                 """,
             expectedFail = DefaultLintErrorMessage,
             sourceFiles =
@@ -38,13 +45,19 @@ class ManagerCheckTest : DriverTest() {
                         """
                             package android.pkg;
 
+                            import androidx.annotation.NonNull;
                             import androidx.annotation.Nullable;
+                            import java.util.List;
 
                             public class MyFirstManager {
                                 public MyFirstManager() {
                                 }
                                 @Nullable
-                                public MyFirstManager get() { return null; }
+                                public MyFirstManager error1() { return null; }
+                                @NonNull
+                                public MyFirstManager[] error2() { return null; }
+                                @NonNull
+                                public List<MyFirstManager> error3() { return null; }
                                 @Nullable
                                 public MySecondManager ok() { return null; }
                             }
@@ -54,12 +67,16 @@ class ManagerCheckTest : DriverTest() {
                         """
                             package android.pkg;
 
-                            public class MySecondManager {
+                            public class MySecondManager<T extends MySecondManager<T>> {
                                 private MySecondManager() {
                                 }
+
+                                @Nullable
+                                public T error1() { return null; }
                             }
                         """
                     ),
+                    androidxNonNullSource,
                     androidxNullableSource,
                 ),
         )
