076a11632371826fe57f276a1ed4179ac859a03a
Paul Duffin <paulduffin@google.com>
2026-02-17 16:18:44 +0000

    Replace PsiSourceParserTest with SourceSetTest
    Previously, `PsiSourceParserTest` had some `SourceSet` tests that only
    ran on `Psi`. That had two main issues:
    1. The `SourceSet` was not tested in `metalava-model-source` making it
       easier to add changes that broke it.
    2. They created Psi Codebases which made the tests expensive to run and
       it was not necessary as `SourceSet` does not use an Psi or Codebase
       specific functionality.
    
    This change moves the tests into their own `SourceSetTest` and
    simplifies them. This is in preparation for making future changes to
    `SourceSet`.
    
    Bug: 479907812
    Test: ./gradlew
    Change-Id: I9f0ccc7e95c8b5003fda3b1e6953be1efd60f996

---
+++ metalava-model-source/src/test/java/com/android/tools/metalava/model/source/SourceSetTest.kt
@@ -0,0 +1,119 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.tools.metalava.model.source
+
+import com.android.tools.metalava.reporter.RecordingReporter
+import com.android.tools.metalava.testing.TemporaryFolderOwner
+import com.android.tools.metalava.testing.java
+import com.android.tools.metalava.testing.kotlin
+import kotlin.test.assertEquals
+import org.junit.After
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+
+class SourceSetTest : TemporaryFolderOwner {
+    /** Provides access to temporary files. */
+    @get:Rule override val temporaryFolder = TemporaryFolder()
+
+    private val reporter = RecordingReporter()
+
+    private fun assertAndRemoveReportedIssues(expectedIssues: String) {
+        val issues = replaceFileWithSymbol(reporter.removeIssues())
+        assertEquals(expectedIssues.trimIndent(), issues.trimIndent())
+    }
+
+    @After
+    fun tearDown() {
+        // Make sure that no unexpected issues were reported.
+        assertAndRemoveReportedIssues("")
+    }
+
+    @Suppress("DanglingJavadoc")
+    @Test
+    fun `Regression test for 124333557`() {
+        // Regression test for 124333557: Handle empty java files
+        val testFiles =
+            listOf(
+                java(
+                    "src/test/pkg/Something.java",
+                    """
+                        /** Nothing much here */
+                    """
+                ),
+                java(
+                    "src/test/pkg/Something2.java",
+                    """
+                        /** Nothing much here */
+                        package test.pkg;
+                    """
+                ),
+                java(
+                    "src/test/Something2.java",
+                    """
+                        /** Wrong package */
+                        package test.wrong;
+                    """
+                ),
+                java(
+                    """
+                        package test.pkg;
+                        public class Test {
+                            private Test() { }
+                        }
+                    """
+                ),
+            )
+
+        testFiles.forEach { it.toFile() }
+
+        // Make sure we handle blank/doc-only java doc files in root extraction.
+        val srcDir = listOf(temporaryFolder.root.resolve("src"))
+        val sourceSet = SourceSet.createFromSourcePath(reporter, srcDir)
+        val roots = sourceSet.extractRoots(reporter).sourcePath
+        assertEquals(srcDir, roots)
+
+        assertAndRemoveReportedIssues(
+            """
+                TESTROOT/src/test/Something2.java: error: Unable to determine the package name. This usually means that a source file was where the directory does not seem to match the package declaration; we expected the path TESTROOT/src/test/Something2.java to end with /test/wrong/Something2.java [IoError]
+            """
+        )
+    }
+
+    @Test
+    fun `Regression test for 359909520`() {
+        // Regression test for 359909520: Handle kotlin packages that have `` in them.
+        val testFiles =
+            listOf(
+                kotlin(
+                    "com/google/receiver/Test.kt",
+                    """
+                        package com.google.`receiver`
+                        class Test
+                    """
+                ),
+            )
+
+        testFiles.forEach { it.toFile() }
+
+        val src = listOf(temporaryFolder.root.resolve("src"))
+        val sourceSet = SourceSet.createFromSourcePath(reporter, src)
+        val roots = sourceSet.extractRoots(reporter).sourcePath
+        assertEquals(1, roots.size)
+        assertEquals(src[0].path, roots[0].path)
+    }
+}
