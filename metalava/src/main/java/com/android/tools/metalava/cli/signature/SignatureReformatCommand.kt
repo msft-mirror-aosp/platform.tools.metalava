7180672b113db09ea1d49b84a80606c68eaa6a79
Paul Duffin <paulduffin@google.com>
2026-03-12 18:37:41 +0000

    Extract FileStructurePreserver from SignatureReformatCommand
    Preparation for a follow-up change that will switch it to be type safe.
    
    Bug: 489071739
    Test: ./gradlew
    Change-Id: I744eb9cdc7453bbc6cfb88fb4d5ee35c91abef62

--- metalava/src/main/java/com/android/tools/metalava/cli/signature/SignatureReformatCommand.kt
+++ metalava/src/main/java/com/android/tools/metalava/cli/signature/SignatureReformatCommand.kt
@@ -98,9 +98,9 @@ class SignatureReformatCommand :
     /**
      * Compute the output [FileFormat] to use.
      *
-     * Returns [targetFormat] unless [preserveStructure] is `true` in which case this will call
-     * [computeStructurePreservingFormat] to create a [FileFormat] from [targetFormat] with settings
-     * from [currentFormat] needed to preserve the structure.
+     * Returns [targetFormat] unless [preserveStructure] is `true` in which case this will use
+     * [FileStructurePreserver] to create a [FileFormat] from [targetFormat] with settings from
+     * [currentFormat] needed to preserve the structure.
      *
      * @param currentFormat the current [FileFormat] for the file.
      * @param targetFormat the target [FileFormat] to which the file is to be reformatted.
@@ -118,7 +118,8 @@ class SignatureReformatCommand :
             val currentFormatWithDefaults = formatOptions.applyDefaultsTo(currentFormat)
 
             // Compute structure preserving format.
-            computeStructurePreservingFormat(currentFormatWithDefaults, targetFormat, codebase)
+            FileStructurePreserver(currentFormatWithDefaults, targetFormat, codebase)
+                .computeFormat()
         } else {
             targetFormat
         }
@@ -142,6 +143,24 @@ class SignatureReformatCommand :
             file.printWriter().use { writer -> writeSignatureFile(codebase, outputFormat, writer) }
         }
     }
+}
+
+/**
+ * Encapsulates logic to compute the [FileFormat] that will preserve the file structure of
+ * [codebase] formatted with [currentFormat] while setting as many of the properties in
+ * [targetFormat] as possible.
+ */
+private class FileStructurePreserver(
+    private val currentFormat: FileFormat,
+    private val targetFormat: FileFormat,
+    private val codebase: Codebase,
+) {
+    /**
+     * The current contents of the signature file against which the effect of property settings will
+     * be compared. Initialized lazily as it can be expensive to compute and may not be needed.
+     */
+    private val currentContents by
+        lazy(LazyThreadSafetyMode.NONE) { codebase.toSignatureNoHeader(currentFormat) }
 
     /**
      * Format this [Codebase] as a signature file using [format] but without the header, returning
@@ -166,19 +185,10 @@ class SignatureReformatCommand :
      * structure by generated signature file contents with and without that setting and comparing
      * the result.
      */
-    private fun computeStructurePreservingFormat(
-        currentFormat: FileFormat,
-        targetFormat: FileFormat,
-        codebase: Codebase,
-    ) =
+    fun computeFormat() =
         // Create a new FileFormat based on the [targetFormat] with properties copied from
         // [currentFormat] where necessary.
         targetFormat.buildCopy {
-            // The current contents of the signature file against which the effect of property
-            // settings will be compared. Initialized lazily as it can be expensive to compute and
-            // may not be needed.
-            val currentContents by
-                lazy(LazyThreadSafetyMode.NONE) { codebase.toSignatureNoHeader(currentFormat) }
 
             // Iterate over all the properties checking to see if the [targetFormat] value needs to
             // be replaced with the [currentFormat] value.
