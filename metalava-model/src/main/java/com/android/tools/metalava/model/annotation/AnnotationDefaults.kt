c8bbf2272a928d21cfd3392a6e5268f927e493c9
Paul Duffin <paulduffin@google.com>
2026-04-03 12:13:39 +0000

    Add equals/hashCode/toString to AnnotationDefaults
    Preparation for a follow-up change that will use `AnnotationDefaults`
    as part of a cache key.
    
    Bug: 499055608
    Test: ./gradlew
    Change-Id: I5f50d5eb3baeb5661c2f75eefdb98a8b12765df7

--- metalava-model/src/main/java/com/android/tools/metalava/model/annotation/AnnotationDefaults.kt
+++ metalava-model/src/main/java/com/android/tools/metalava/model/annotation/AnnotationDefaults.kt
@@ -37,6 +37,19 @@ class AnnotationDefaults(private val defaultsByName: Map<String, Value>) {
         }
     }
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as AnnotationDefaults
+
+        return defaultsByName == other.defaultsByName
+    }
+
+    override fun hashCode() = defaultsByName.hashCode()
+
+    override fun toString() = "AnnotationDefaults($defaultsByName)"
+
     companion object {
         val EMPTY = AnnotationDefaults(emptyMap())
     }
