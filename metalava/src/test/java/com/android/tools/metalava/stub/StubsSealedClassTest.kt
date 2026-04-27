2eb3f8f799f828e683550ea2c515973d353b64e5
Paul Duffin <paulduffin@google.com>
2026-04-27 13:00:02 +0000

    Add tests to show current contents of stub sealed classes
    Adds tests to cover the following cases:
    * Abstract sealed classes, with non-default constructor.
    * Concrete sealed classes, with non-default constructor.
    * Sealed interfaces.
    
    All of the above have an `exhaustive` and `non-exhaustive` version of
    the test.
    
    The tests show that stub sealed classes/interfaces are not currently
    generated correctly. That will be fixed in a follow-up change.
    
    Bug: 482391240
    Test: ./gradlew
    Change-Id: I38d0991826948a1d3e94449e0db2af17f8ece7ad

---
+++ metalava/src/test/java/com/android/tools/metalava/stub/StubsSealedClassTest.kt
@@ -0,0 +1,454 @@
+/*
+ * Copyright (C) 2026 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.tools.metalava.stub
+
+import com.android.tools.metalava.model.text.FORMAT_V6_WITH_JAVA_SEALED_CLASSES
+import com.android.tools.metalava.testing.java
+import org.junit.Test
+
+class StubsSealedClassTest : AbstractStubsTest() {
+    @Test
+    fun `Test exhaustive abstract sealed class with java-sealed-classes=yes`() {
+        checkStubs(
+            format = FORMAT_V6_WITH_JAVA_SEALED_CLASSES,
+            sourceFiles =
+                arrayOf(
+                    java(
+                        """
+                            package test.pkg;
+
+                            public abstract sealed class Sealed {
+                                public Sealed(int a) {}
+                            }
+                        """
+                    ),
+                    java(
+                        """
+                            package test.pkg;
+
+                            public final class SubclassA extends Sealed {
+                            }
+                        """
+                    ),
+                    java(
+                        """
+                            package test.pkg;
+
+                            public non-sealed class SubclassB extends Sealed {
+                            }
+                        """
+                    ),
+                ),
+            // TODO(b/482391240): Remove when issues are fixed.
+            checkTextStubEquivalence = false,
+            api =
+                """
+                    // Signature format: 6.0
+                    // - style=java
+                    // - java-sealed-classes=yes
+                    package test.pkg {
+                      public abstract sealed exhaustive class Sealed permits test.pkg.SubclassA test.pkg.SubclassB {
+                      }
+                      public final class SubclassA extends test.pkg.Sealed {
+                        ctor public SubclassA();
+                      }
+                      public non-sealed class SubclassB extends test.pkg.Sealed {
+                        ctor public SubclassB();
+                      }
+                    }
+                """,
+            stubFiles =
+                arrayOf(
+                    java(
+                        """
+                            package test.pkg;
+                            @SuppressWarnings({"unchecked", "deprecation", "all"})
+                            public abstract class Sealed {
+                            Sealed() { throw new RuntimeException("Stub!"); }
+                            }
+                        """
+                    ),
+                    java(
+                        """
+                            package test.pkg;
+                            @SuppressWarnings({"unchecked", "deprecation", "all"})
+                            public final class SubclassA extends test.pkg.Sealed {
+                            public SubclassA() { throw new RuntimeException("Stub!"); }
+                            }
+                        """
+                    ),
+                    java(
+                        """
+                            package test.pkg;
+                            @SuppressWarnings({"unchecked", "deprecation", "all"})
+                            public class SubclassB extends test.pkg.Sealed {
+                            public SubclassB() { throw new RuntimeException("Stub!"); }
+                            }
+                        """
+                    ),
+                ),
+        )
+    }
+
+    @Test
+    fun `Test non-exhaustive abstract sealed class with java-sealed-classes=yes`() {
+        checkStubs(
+            format = FORMAT_V6_WITH_JAVA_SEALED_CLASSES,
+            sourceFiles =
+                arrayOf(
+                    java(
+                        """
+                            package test.pkg;
+
+                            public abstract sealed class Sealed {
+                                public Sealed(int a) {}
+                            }
+                        """
+                    ),
+                    java(
+                        """
+                            package test.pkg;
+
+                            public final class SubclassA extends Sealed {
+                            }
+                        """
+                    ),
+                    java(
+                        """
+                            package test.pkg;
+
+                            public non-sealed class SubclassB extends Sealed {
+                            }
+                        """
+                    ),
+                    java(
+                        """
+                            package test.pkg;
+
+                            non-sealed class SubclassPrivate extends Sealed {
+                            }
+                        """
+                    ),
+                ),
+            // TODO(b/482391240): Remove when issues are fixed.
+            checkTextStubEquivalence = false,
+            // TODO(b/482391240): Remove test.pkg.SubclassPrivate from the permits list.
+            api =
+                """
+                    // Signature format: 6.0
+                    // - style=java
+                    // - java-sealed-classes=yes
+                    package test.pkg {
+                      public abstract sealed non-exhaustive class Sealed permits test.pkg.SubclassA test.pkg.SubclassB test.pkg.SubclassPrivate {
+                      }
+                      public final class SubclassA extends test.pkg.Sealed {
+                        ctor public SubclassA();
+                      }
+                      public non-sealed class SubclassB extends test.pkg.Sealed {
+                        ctor public SubclassB();
+                      }
+                    }
+                """,
+            stubFiles =
+                arrayOf(
+                    java(
+                        """
+                            package test.pkg;
+                            @SuppressWarnings({"unchecked", "deprecation", "all"})
+                            public abstract class Sealed {
+                            Sealed() { throw new RuntimeException("Stub!"); }
+                            }
+                        """
+                    ),
+                    java(
+                        """
+                            package test.pkg;
+                            @SuppressWarnings({"unchecked", "deprecation", "all"})
+                            public final class SubclassA extends test.pkg.Sealed {
+                            public SubclassA() { throw new RuntimeException("Stub!"); }
+                            }
+                        """
+                    ),
+                    java(
+                        """
+                            package test.pkg;
+                            @SuppressWarnings({"unchecked", "deprecation", "all"})
+                            public class SubclassB extends test.pkg.Sealed {
+                            public SubclassB() { throw new RuntimeException("Stub!"); }
+                            }
+                        """
+                    ),
+                ),
+        )
+    }
+
+    @Test
+    fun `Test non-exhaustive sealed class with java-sealed-classes=yes`() {
+        checkStubs(
+            format = FORMAT_V6_WITH_JAVA_SEALED_CLASSES,
+            sourceFiles =
+                arrayOf(
+                    java(
+                        """
+                            package test.pkg;
+
+                            public sealed class Sealed {
+                                public Sealed(int a) {}
+                            }
+                        """
+                    ),
+                    java(
+                        """
+                            package test.pkg;
+
+                            public final class SubclassA extends Sealed {
+                            }
+                        """
+                    ),
+                    java(
+                        """
+                            package test.pkg;
+
+                            public non-sealed class SubclassB extends Sealed {
+                            }
+                        """
+                    ),
+                    java(
+                        """
+                            package test.pkg;
+
+                            non-sealed class SubclassPrivate extends Sealed {
+                            }
+                        """
+                    ),
+                ),
+            // TODO(b/482391240): Remove when issues are fixed.
+            checkTextStubEquivalence = false,
+            // TODO(b/482391240): Remove test.pkg.SubclassPrivate from the permits list.
+            api =
+                """
+                    // Signature format: 6.0
+                    // - style=java
+                    // - java-sealed-classes=yes
+                    package test.pkg {
+                      public sealed non-exhaustive class Sealed permits test.pkg.SubclassA test.pkg.SubclassB test.pkg.SubclassPrivate {
+                        ctor public Sealed(int);
+                      }
+                      public final class SubclassA extends test.pkg.Sealed {
+                        ctor public SubclassA();
+                      }
+                      public non-sealed class SubclassB extends test.pkg.Sealed {
+                        ctor public SubclassB();
+                      }
+                    }
+                """,
+            stubFiles =
+                arrayOf(
+                    java(
+                        """
+                            package test.pkg;
+                            @SuppressWarnings({"unchecked", "deprecation", "all"})
+                            public class Sealed {
+                            public Sealed(int a) { throw new RuntimeException("Stub!"); }
+                            }
+                        """
+                    ),
+                    java(
+                        """
+                            package test.pkg;
+                            @SuppressWarnings({"unchecked", "deprecation", "all"})
+                            public final class SubclassA extends test.pkg.Sealed {
+                            public SubclassA() { super(0); throw new RuntimeException("Stub!"); }
+                            }
+                        """
+                    ),
+                    java(
+                        """
+                            package test.pkg;
+                            @SuppressWarnings({"unchecked", "deprecation", "all"})
+                            public class SubclassB extends test.pkg.Sealed {
+                            public SubclassB() { super(0); throw new RuntimeException("Stub!"); }
+                            }
+                        """
+                    ),
+                ),
+        )
+    }
+
+    @Test
+    fun `Test exhaustive sealed interface with java-sealed-classes=yes`() {
+        checkStubs(
+            format = FORMAT_V6_WITH_JAVA_SEALED_CLASSES,
+            sourceFiles =
+                arrayOf(
+                    java(
+                        """
+                            package test.pkg;
+
+                            public sealed interface Sealed {
+                            }
+                        """
+                    ),
+                    java(
+                        """
+                            package test.pkg;
+
+                            public non-sealed interface SubclassA extends Sealed {
+                            }
+                        """
+                    ),
+                    java(
+                        """
+                            package test.pkg;
+
+                            public non-sealed interface SubclassB extends Sealed {
+                            }
+                        """
+                    ),
+                ),
+            // TODO(b/482391240): Remove when issues are fixed.
+            checkTextStubEquivalence = false,
+            api =
+                """
+                    // Signature format: 6.0
+                    // - style=java
+                    // - java-sealed-classes=yes
+                    package test.pkg {
+                      public sealed exhaustive interface Sealed permits test.pkg.SubclassA test.pkg.SubclassB {
+                      }
+                      public non-sealed interface SubclassA extends test.pkg.Sealed {
+                      }
+                      public non-sealed interface SubclassB extends test.pkg.Sealed {
+                      }
+                    }
+                """,
+            stubFiles =
+                arrayOf(
+                    java(
+                        """
+                            package test.pkg;
+                            @SuppressWarnings({"unchecked", "deprecation", "all"})
+                            public interface Sealed {
+                            }
+                        """
+                    ),
+                    java(
+                        """
+                            package test.pkg;
+                            @SuppressWarnings({"unchecked", "deprecation", "all"})
+                            public interface SubclassA extends test.pkg.Sealed {
+                            }
+                        """
+                    ),
+                    java(
+                        """
+                            package test.pkg;
+                            @SuppressWarnings({"unchecked", "deprecation", "all"})
+                            public interface SubclassB extends test.pkg.Sealed {
+                            }
+                        """
+                    ),
+                ),
+        )
+    }
+
+    @Test
+    fun `Test non-exhaustive sealed interface with java-sealed-classes=yes`() {
+        checkStubs(
+            format = FORMAT_V6_WITH_JAVA_SEALED_CLASSES,
+            sourceFiles =
+                arrayOf(
+                    java(
+                        """
+                            package test.pkg;
+
+                            public sealed interface Sealed {
+                            }
+                        """
+                    ),
+                    java(
+                        """
+                            package test.pkg;
+
+                            public non-sealed interface SubclassA extends Sealed {
+                            }
+                        """
+                    ),
+                    java(
+                        """
+                            package test.pkg;
+
+                            public non-sealed interface SubclassB extends Sealed {
+                            }
+                        """
+                    ),
+                    java(
+                        """
+                            package test.pkg;
+
+                            non-sealed interface SubclassPrivate extends Sealed {
+                            }
+                        """
+                    ),
+                ),
+            // TODO(b/482391240): Remove when issues are fixed.
+            checkTextStubEquivalence = false,
+            // TODO(b/482391240): Remove test.pkg.SubclassPrivate from the permits list.
+            api =
+                """
+                    // Signature format: 6.0
+                    // - style=java
+                    // - java-sealed-classes=yes
+                    package test.pkg {
+                      public sealed non-exhaustive interface Sealed permits test.pkg.SubclassA test.pkg.SubclassB test.pkg.SubclassPrivate {
+                      }
+                      public non-sealed interface SubclassA extends test.pkg.Sealed {
+                      }
+                      public non-sealed interface SubclassB extends test.pkg.Sealed {
+                      }
+                    }
+                """,
+            stubFiles =
+                arrayOf(
+                    java(
+                        """
+                            package test.pkg;
+                            @SuppressWarnings({"unchecked", "deprecation", "all"})
+                            public interface Sealed {
+                            }
+                        """
+                    ),
+                    java(
+                        """
+                            package test.pkg;
+                            @SuppressWarnings({"unchecked", "deprecation", "all"})
+                            public interface SubclassB extends test.pkg.Sealed {
+                            }
+                        """
+                    ),
+                    java(
+                        """
+                            package test.pkg;
+                            @SuppressWarnings({"unchecked", "deprecation", "all"})
+                            public interface SubclassA extends test.pkg.Sealed {
+                            }
+                        """
+                    ),
+                ),
+        )
+    }
+}
