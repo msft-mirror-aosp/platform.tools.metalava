6305c6d25da24e25728e9716fb82b2a2d4f78727
Paul Duffin <paulduffin@google.com>
2026-04-29 14:09:00 +0000

    Add tests for `sealed` class when java-sealed-classes=no
    Shows that the modifier is just dropped. A follow-up change will make
    this report an issue.
    
    Bug: 482391240
    Test: ./gradlew
    Change-Id: I2be0305f4d710f9e501fedf47b51199607df8a97

---
+++ metalava/src/test/java/com/android/tools/metalava/SealedClassTest.kt
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2026 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.tools.metalava
+
+import com.android.tools.metalava.model.text.FORMAT_V6_WITHOUT_JAVA_SEALED_CLASSES
+import com.android.tools.metalava.testing.java
+import org.junit.Test
+
+class SealedClassTest : DriverTest() {
+    @Test
+    fun `disallow sealed interface when java-sealed-classes=no`() {
+        check(
+            format = FORMAT_V6_WITHOUT_JAVA_SEALED_CLASSES,
+            sourceFiles =
+                arrayOf(
+                    java(
+                        """
+                            package test.pkg;
+
+                            public sealed interface Test {}
+                        """
+                    )
+                ),
+            api =
+                """
+                    package test.pkg {
+                      public interface Test {
+                      }
+                    }
+                """,
+            // TODO(b/482391240): Should be reporting an issue about sealed interfaces being
+            //  disallowed.
+            expectedIssues = "",
+        )
+    }
+
+    @Test
+    fun `disallow sealed class when java-sealed-classes=no`() {
+        check(
+            format = FORMAT_V6_WITHOUT_JAVA_SEALED_CLASSES,
+            sourceFiles =
+                arrayOf(
+                    java(
+                        """
+                            package test.pkg;
+
+                            public sealed class Test {}
+                        """
+                    )
+                ),
+            api =
+                """
+                    package test.pkg {
+                      public class Test {
+                        ctor public Test();
+                      }
+                    }
+                """,
+            // TODO(b/482391240): Should be reporting an issue about sealed interfaces being
+            //  disallowed.
+            expectedIssues = "",
+        )
+    }
+}
