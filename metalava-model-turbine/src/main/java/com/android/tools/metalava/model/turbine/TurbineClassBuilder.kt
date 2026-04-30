db4b05061caaa41fe4ddc347be1933d599b328ff
Paul Duffin <paulduffin@google.com>
2026-04-30 11:52:41 +0000

    Correct comments when setting exhaustivity = true
    Bug: 482391240
    Test: ./gradlew
    Change-Id: I113db88b1ed8ff59ab337fa55095d8618a0e81f2

--- metalava-model-turbine/src/main/java/com/android/tools/metalava/model/turbine/TurbineClassBuilder.kt
+++ metalava-model-turbine/src/main/java/com/android/tools/metalava/model/turbine/TurbineClassBuilder.kt
@@ -259,7 +259,7 @@ internal class TurbineClassBuilder(
             }
         modifiers.setDeprecated(isDeprecated(annotations))
 
-        // Set exhaustivity as true until proven otherwise either by an inaccessible subclass.
+        // Set exhaustivity as true until proven otherwise by an inaccessible subclass.
         if (modifiers.isSealed()) {
             modifiers.setExhaustive(true)
         }
