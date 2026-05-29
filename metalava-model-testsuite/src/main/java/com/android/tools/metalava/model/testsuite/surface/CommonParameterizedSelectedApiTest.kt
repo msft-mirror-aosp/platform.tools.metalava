eafb48352d06ee333d43f2c24e9a9d31e07e016b
Paul Duffin <paulduffin@google.com>
2026-05-29 16:28:46 +0000

    Add infrastructure for testing selected apis behavior
    This changes adds `CommonParameterizedSelectedApiTest` to test how
    `SelectableItem`s are selected for different API surfaces and
    `TestableApiSurfaces` to provide some common API surface related
    objects used for testing.
    
    Currently, the test does not work as expected. Follow-up changes will
    rectify that and incrementally expand the set of tests.
    
    Bug: 512093496
    Test: ./gradlew
    Change-Id: I06d230a6eebad6a2b8dd34e5cb8b6b2227af0bd2

---
+++ metalava-model-testsuite/src/main/java/com/android/tools/metalava/model/testsuite/surface/CommonParameterizedSelectedApiTest.kt
@@ -0,0 +1,244 @@
+/*
+ * Copyright (C) 2026 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.tools.metalava.model.testsuite.surface
+
+import com.android.tools.lint.checks.infrastructure.TestFile
+import com.android.tools.metalava.model.Codebase
+import com.android.tools.metalava.model.api.ApiSurfaceRules
+import com.android.tools.metalava.model.provider.InputFormat
+import com.android.tools.metalava.model.testing.SupportedInputFormats
+import com.android.tools.metalava.model.testing.surfaces.TestableApiSurfaces.UNANNOTATED_API
+import com.android.tools.metalava.model.testing.surfaces.TestableApiSurfaces.UNANNOTATED_NON_RECURSIVE_API
+import com.android.tools.metalava.model.testing.surfaces.TestableApiSurfaces.annotatedOnlyRules
+import com.android.tools.metalava.model.testing.surfaces.TestableApiSurfaces.publicSystemModuleRules
+import com.android.tools.metalava.model.testsuite.BaseModelTest
+import com.android.tools.metalava.testing.EntryPoint
+import com.android.tools.metalava.testing.EntryPointCallerRule
+import com.android.tools.metalava.testing.EntryPointCallerTracker
+import com.android.tools.metalava.testing.ExitPoint
+import com.android.tools.metalava.testing.java
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runners.Parameterized
+
+@SupportedInputFormats(InputFormat.JAVA)
+class CommonParameterizedSelectedApiTest : BaseModelTest() {
+
+    @Parameterized.Parameter(0) internal lateinit var params: TestParams
+
+    /**
+     * Will try and rewrite the stack trace of any test failures to refer to the location where the
+     * [TestParams] that is currently being tested was created.
+     */
+    @get:Rule val entryPointCallerRule = EntryPointCallerRule { params.entryPointCallerTracker }
+
+    data class TestParams
+    @EntryPoint
+    constructor(
+        val name: String,
+        val surfaceRules: ApiSurfaceRules,
+        val sources: List<TestFile>,
+        val surface: String,
+        val expected: String,
+    ) {
+        /**
+         * Record the stack trace of the creation of this which can be used to provide a stack trace
+         * to the creator of this instance in the event of a test failure.
+         */
+        val entryPointCallerTracker = EntryPointCallerTracker()
+
+        override fun toString() = name
+    }
+
+    companion object {
+        /**
+         * Build [TestParams] and add them to this list.
+         *
+         * @param name the [TestParams.name].
+         * @param surfaceRules the [TestParams.surfaceRules].
+         * @param sources the [TestParams.sources].
+         * @param body lambda that will add tests for specific surfaces using [Builder.surfaceTest]
+         *   which creates a [TestParams] using the above plus some surface specific information.
+         */
+        @EntryPoint
+        fun MutableList<TestParams>.buildTests(
+            name: String,
+            surfaceRules: ApiSurfaceRules,
+            sources: List<TestFile>,
+            body: Builder.() -> Unit,
+        ) {
+            val builder = Builder(this, name, surfaceRules, sources)
+            buildSurfaceTests(builder, body)
+        }
+
+        /**
+         * Invokes [body] on [builder].
+         *
+         * Separated out as per instructions in [ExitPoint].
+         */
+        @ExitPoint
+        fun buildSurfaceTests(builder: Builder, body: Builder.() -> Unit) {
+            builder.body()
+        }
+
+        /** Builder of [TestParams]. */
+        class Builder(
+            private val params: MutableList<TestParams>,
+            private val name: String,
+            private val surfaceRules: ApiSurfaceRules,
+            private val sources: List<TestFile>,
+        ) {
+            /**
+             * Create a test for [surface] that expects [expected] to be the result of calling
+             * [Codebase.assertSelectedApiVariants].
+             */
+            @EntryPoint
+            fun surfaceTest(surface: String, expected: String) {
+                params.add(TestParams("$name/$surface", surfaceRules, sources, surface, expected))
+            }
+        }
+
+        @JvmStatic
+        @Parameterized.Parameters
+        fun params() = buildList {
+            buildTests(
+                name = "basic class",
+                surfaceRules = publicSystemModuleRules,
+                sources =
+                    listOf(
+                        java(
+                            """
+                                package test.pkg;
+                                public interface Test {
+                                }
+                            """
+                        ),
+                    ),
+            ) {
+                surfaceTest(
+                    surface = "public",
+                    expected =
+                        // TODO(b/512093496): The test.pkg and test.pkg.Test should be in the core
+                        //   public API, i.e. "public(C)".
+                        """
+                            package test.pkg - ApiVariantSet[]
+                              class test.pkg.Test - ApiVariantSet[]
+                        """,
+                )
+            }
+
+            buildTests(
+                name = "@hide doctag class",
+                surfaceRules = publicSystemModuleRules,
+                sources =
+                    listOf(
+                        java(
+                            """
+                                package test;
+                                /** @hide */
+                                public interface Hidden {
+                                }
+                            """
+                        ),
+                    ),
+            ) {
+                surfaceTest(
+                    surface = "public",
+                    expected =
+                        """
+                            package test - ApiVariantSet[]
+                              class test.Hidden - ApiVariantSet[]
+                        """,
+                )
+            }
+
+            buildTests(
+                name = "annotated only",
+                surfaceRules = annotatedOnlyRules,
+                sources =
+                    listOf(
+                        java(
+                            """
+                                package test.pkg;
+                            """
+                        ),
+                        java(
+                            """
+                                package test.pkg;
+                                $UNANNOTATED_API
+                                public class Test {
+                                    public Test(int a) {}
+
+                                    public void method() {}
+                                }
+                            """
+                        ),
+                        java(
+                            """
+                                package test.pkg;
+                                $UNANNOTATED_NON_RECURSIVE_API
+                                public class ClassOnly {
+                                    public ClassOnly() {}
+
+                                    public void notIncluded() {}
+                                }
+                            """
+                        ),
+                        java(
+                            """
+                                package test.pkg;
+                                public interface Unannotated {
+                                }
+                            """
+                        ),
+                    ),
+            ) {
+                surfaceTest(
+                    surface = "public",
+                    expected =
+                        // TODO(b/512093496): Everything apart from Unannotated should be in the
+                        //   core public API, i.e. "public(C)".
+                        """
+                            package test.pkg - ApiVariantSet[]
+                              class test.pkg.Test - ApiVariantSet[]
+                                constructor test.pkg.Test(int) - ApiVariantSet[]
+                                method test.pkg.Test.method() - ApiVariantSet[]
+                              class test.pkg.ClassOnly - ApiVariantSet[]
+                                constructor test.pkg.ClassOnly() - ApiVariantSet[]
+                                method test.pkg.ClassOnly.notIncluded() - ApiVariantSet[]
+                              class test.pkg.Unannotated - ApiVariantSet[]
+                        """,
+                )
+            }
+        }
+    }
+
+    @Test
+    fun `Test selected api variants`() {
+        val rules = params.surfaceRules.retargetAt(params.surface)
+
+        runCodebaseTest(
+            inputSet(params.sources),
+            testFixture =
+                TestFixture(
+                    apiSurfaceRules = rules,
+                ),
+        ) {
+            codebase.assertSelectedApiVariants(params.expected)
+        }
+    }
+}
