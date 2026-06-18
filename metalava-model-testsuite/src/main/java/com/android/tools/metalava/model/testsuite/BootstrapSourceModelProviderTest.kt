2e6b8e3f0bdde5c071d2035d8a0e9372ad148be9
Paul Duffin <paulduffin@google.com>
2026-06-18 12:32:42 +0000

    Migrate doc-only tests that required class-level design adjustments
    Now that doc-only behavior relies on annotations rather than Javadoc
    comments, some tests require design adjustments because the doc-only
    annotation is only supported/applied to class items (not directly on
    packages, methods, or fields).
    
    Specifically:
    - Refactored `CommonApiVariantSelectorsTest.Test docOnly` to test
      outer-class to inner-class inheritance using
      `KnownSourceFiles.docOnlyAnnotation` (since package-level
      doc-only is not supported by the new annotation-based design).
    - Refactored `BootstrapSourceModelProviderTest.260 - test doconly
      members` to use `KnownSourceFiles.docOnlyAnnotation` and updated
      the direct field doc-only assertion to expect false.
    
    Bug: 519195092
    Test: ./gradlew test
    Change-Id: I7b835ddcf0f70fbaf64605c5dae4474099eb430f

--- metalava-model-testsuite/src/main/java/com/android/tools/metalava/model/testsuite/BootstrapSourceModelProviderTest.kt
+++ metalava-model-testsuite/src/main/java/com/android/tools/metalava/model/testsuite/BootstrapSourceModelProviderTest.kt
@@ -21,6 +21,9 @@ import com.android.tools.metalava.model.AnnotationRetention
 import com.android.tools.metalava.model.ClassTypeItem
 import com.android.tools.metalava.model.PrimitiveTypeItem
 import com.android.tools.metalava.model.VariableTypeItem
+import com.android.tools.metalava.model.api.ApiSurfaceRules
+import com.android.tools.metalava.model.api.SurfaceSelectionRule
+import com.android.tools.metalava.model.api.surface.ApiSurfaces
 import com.android.tools.metalava.model.noOpAnnotationManager
 import com.android.tools.metalava.model.provider.InputFormat
 import com.android.tools.metalava.model.testing.SupportedInputFormats
@@ -31,6 +34,7 @@ import com.android.tools.metalava.model.testing.value.arrayValueFromAny
 import com.android.tools.metalava.model.testing.value.classObjectValue
 import com.android.tools.metalava.model.testing.value.fieldReferenceValue
 import com.android.tools.metalava.model.value.asInt
+import com.android.tools.metalava.testing.KnownSourceFiles
 import com.android.tools.metalava.testing.java
 import com.google.common.truth.Truth.assertThat
 import kotlin.test.assertEquals
@@ -1016,22 +1020,38 @@ class BootstrapSourceModelProviderTest : BaseModelTest() {
 
     @Test
     fun `260 - test doconly members`() {
+        val apiSurfaces = ApiSurfaces.create()
+        val rulesByName = mapOf("public" to listOf(SurfaceSelectionRule.unannotated))
+        val variantRules =
+            listOf(
+                SurfaceSelectionRule.createAnnotationRule(
+                    "android.annotation.DocOnly",
+                    effect = SurfaceSelectionRule.Effect.DOC_ONLY,
+                ),
+            )
+        val apiSurfaceRules = ApiSurfaceRules(apiSurfaces, rulesByName, variantRules)
+
         runSourceCodebaseTest(
-            java(
-                """
-                    package test.pkg;
+            inputSet(
+                KnownSourceFiles.docOnlyAnnotation,
+                java(
+                    """
+                        package test.pkg;
+                        import android.annotation.DocOnly;
 
-                    public class Test {
-                        /** @doconly */
-                        public class Inner {
-                            public int InnerField;
+                        public class Test {
+                            @DocOnly
+                            public class Inner {
+                                public int InnerField;
+                            }
+
+                            @DocOnly
+                            public int Field;
                         }
-
-                        /** @doconly Some docs here */
-                        public int Field;
-                    }
-                """
+                    """
+                ),
             ),
+            testFixture = TestFixture(apiSurfaceRules = apiSurfaceRules),
         ) {
             val classItem = codebase.assertClass("test.pkg.Test")
             val classSelectors = classItem.variantSelectors
@@ -1043,7 +1063,7 @@ class BootstrapSourceModelProviderTest : BaseModelTest() {
             assertEquals(false, classSelectors.docOnly, message = "classSelectors.docOnly")
             assertEquals(true, innerClassSelectors.docOnly, message = "innerClassSelectors.docOnly")
             assertEquals(true, innerFieldSelectors.docOnly, message = "innerFieldSelectors.docOnly")
-            assertEquals(true, fieldSelectors.docOnly, message = "fieldSelectors.docOnly")
+            assertEquals(false, fieldSelectors.docOnly, message = "fieldSelectors.docOnly")
         }
     }
 }
