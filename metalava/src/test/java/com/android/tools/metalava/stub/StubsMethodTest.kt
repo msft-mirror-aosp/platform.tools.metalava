ae42cca6a6f09985756f2a35dbca8718c1443ae9
Paul Duffin <paulduffin@google.com>
2026-07-21 11:45:37 +0000

    Add tests for hiding override of SystemApi method
    Add tests that check behavior when hiding a public override of a
    SystemApi method in public API and system API surfaces.
    
    Bug: 537214023
    Test: ./gradlew :metalava:test
    Change-Id: I618ad4ed5b96dbe83b829f1d806d2bf3b74281bf

--- metalava/src/test/java/com/android/tools/metalava/stub/StubsMethodTest.kt
+++ metalava/src/test/java/com/android/tools/metalava/stub/StubsMethodTest.kt
@@ -16,6 +16,7 @@
 
 package com.android.tools.metalava.stub
 
+import com.android.tools.lint.checks.infrastructure.TestFile
 import com.android.tools.metalava.KnownApiSurface
 import com.android.tools.metalava.testing.java
 import org.junit.Test
@@ -53,6 +54,135 @@ class StubsMethodTest : AbstractStubsTest() {
                     package test.pkg {
                       public class Child extends test.pkg.Parent {
                         ctor public Child();
+                      }
+                      public class Parent {
+                        ctor public Parent();
+                        method public void method();
+                      }
+                    }
+                """,
+            stubPaths =
+                arrayOf(
+                    "test/pkg/Child.java",
+                    "test/pkg/Parent.java",
+                ),
+            expectedStubFiles =
+                arrayOf(
+                    java(
+                        """
+                            package test.pkg;
+                            @SuppressWarnings({"unchecked", "deprecation", "all"})
+                            public class Child extends test.pkg.Parent {
+                            public Child() { throw new RuntimeException("Stub!"); }
+                            public void method() { throw new RuntimeException("Stub!"); }
+                            }
+                        """
+                    ),
+                    java(
+                        """
+                            package test.pkg;
+                            @SuppressWarnings({"unchecked", "deprecation", "all"})
+                            public class Parent {
+                            public Parent() { throw new RuntimeException("Stub!"); }
+                            public void method() { throw new RuntimeException("Stub!"); }
+                            }
+                        """
+                    ),
+                ),
+        )
+    }
+
+    /**
+     * Helper to test hiding an override of a `@SystemApi` method.
+     *
+     * Defines a `@SystemApi` class `test.pkg.Parent` with a public method, and a subclass
+     * `test.pkg.Child` that overrides the method and hides it with `@Hide`.
+     */
+    private fun checkPublicOverrideOfSystemApiMethod(
+        apiSurface: KnownApiSurface,
+        expectedIssues: String? = "",
+        expectedApiSignature: String,
+        stubPaths: Array<String>,
+        expectedStubFiles: Array<TestFile>,
+    ) {
+        check(
+            apiSurface = apiSurface,
+            sourceFiles =
+                arrayOf(
+                    java(
+                        """
+                            package test.pkg;
+                            import android.annotation.SystemApi;
+                            @SystemApi
+                            public class Parent {
+                                public void method() {}
+                            }
+                        """
+                    ),
+                    java(
+                        """
+                            package test.pkg;
+                            import android.annotation.Hide;
+                            public class Child extends Parent {
+                                @Hide
+                                @Override
+                                public void method() {}
+                            }
+                        """
+                    ),
+                ),
+            expectedIssues = expectedIssues,
+            expectedApiSignature = expectedApiSignature,
+            stubPaths = stubPaths,
+            expectedStubFiles = expectedStubFiles,
+        )
+    }
+
+    @Test
+    fun `Test hiding override of SystemApi method in public API`() {
+        checkPublicOverrideOfSystemApiMethod(
+            apiSurface = KnownApiSurface.PUBLIC,
+            expectedIssues =
+                """
+                    src/test/pkg/Child.java:3: warning: Public class test.pkg.Child stripped of unavailable superclass test.pkg.Parent [HiddenSuperclass]
+                """,
+            expectedApiSignature =
+                """
+                    // Signature format: 5.0
+                    package test.pkg {
+                      public class Child {
+                        ctor public Child();
+                      }
+                    }
+                """,
+            stubPaths =
+                arrayOf(
+                    "test/pkg/Child.java",
+                ),
+            expectedStubFiles =
+                arrayOf(
+                    java(
+                        """
+                            package test.pkg;
+                            @SuppressWarnings({"unchecked", "deprecation", "all"})
+                            public class Child {
+                            public Child() { throw new RuntimeException("Stub!"); }
+                            }
+                        """
+                    ),
+                ),
+        )
+    }
+
+    @Test
+    fun `Test hiding of public override of SystemApi method in system API`() {
+        checkPublicOverrideOfSystemApiMethod(
+            apiSurface = KnownApiSurface.SYSTEM,
+            expectedApiSignature =
+                """
+                    // Signature format: 5.0
+                    package test.pkg {
+                      public class Child extends test.pkg.Parent {
                       }
                       public class Parent {
                         ctor public Parent();
