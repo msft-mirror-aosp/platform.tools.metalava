5c8bb6a04b10520f8829296097ee78abf491c1e2
Paul Duffin <paulduffin@google.com>
2026-09-11 11:15:51 +0000

    Add tests for overriding methods marked @Hide in OverridingMethodTest
    Add OverridingMethodTest to verify the behavior of overriding methods
    marked @Hide when generating API signature files across API surfaces:
    
    1. A removed system class (@SystemApi, @RemovedFromApi) overrides a
       method from a public interface and marks the overriding method as
       @Hide. In this scenario, RemovedClass belongs to the system API
       surface and its overriding method is hidden, so neither is emitted
       into the module-lib removed API surface even when
       add-additional-overrides=yes.
    
    2. A system class (@SystemApi) extends a removed public class
       (@RemovedFromApi) and overrides a method marked @Hide. In this
       scenario, SystemClass belongs to the system API surface and does not
       inherit the removed status from the super method, so neither the
       class nor the method is emitted into the module-lib removed API
       surface even when add-additional-overrides=yes.
    
    This improves test coverage in preparation for follow up changes.
    
    Bug: 512093496
    Test: ./gradlew :metalava:test
    Change-Id: I67a975ebca4d16506a505ec5382e8af0e13fd057

---
+++ metalava/src/test/java/com/android/tools/metalava/OverridingMethodTest.kt
@@ -0,0 +1,117 @@
+/*
+ * Copyright (C) 2026 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.tools.metalava
+
+import com.android.tools.metalava.model.text.FileFormat
+import com.android.tools.metalava.testing.java
+import org.junit.Test
+
+class OverridingMethodTest : DriverTest() {
+
+    @Test
+    fun `Test removed class overriding method from public interface marked as @Hide`() {
+        check(
+            apiSurface = KnownApiSurface.MODULE_LIB,
+            extraArguments = arrayOf("--format-defaults", "add-additional-overrides=yes"),
+            format = FileFormat.V2,
+            sourceFiles =
+                arrayOf(
+                    java(
+                        """
+                            package test.pkg;
+
+                            public interface PublicInterface {
+                                void method();
+                            }
+                        """
+                    ),
+                    java(
+                        """
+                            package test.pkg;
+
+                            import android.annotation.Hide;
+                            import android.annotation.RemovedFromApi;
+                            import android.annotation.SystemApi;
+
+                            @RemovedFromApi
+                            @SystemApi
+                            public class RemovedClass implements PublicInterface {
+                                @Hide
+                                @Override
+                                public void method() {}
+                            }
+                        """
+                    ),
+                ),
+            expectedApiSignature = "",
+            // RemovedClass is part of the system API surface (not module-lib) and its
+            // overriding method is marked @Hide, overriding a public interface method.
+            // Therefore, neither the class nor the method is emitted into the module-lib
+            // removed API signature file, even with add-additional-overrides=yes.
+            removedApi = "",
+        )
+    }
+
+    @Test
+    fun `Test system class overriding method from removed public class marked as @Hide`() {
+        check(
+            apiSurface = KnownApiSurface.MODULE_LIB,
+            extraArguments = arrayOf("--format-defaults", "add-additional-overrides=yes"),
+            format = FileFormat.V2,
+            sourceFiles =
+                arrayOf(
+                    java(
+                        """
+                            package test.pkg;
+
+                            import android.annotation.RemovedFromApi;
+
+                            @RemovedFromApi
+                            public class RemovedPublicClass {
+                                public void method() {}
+                            }
+                        """
+                    ),
+                    java(
+                        """
+                            package test.pkg;
+
+                            import android.annotation.Hide;
+                            import android.annotation.SystemApi;
+
+                            @SystemApi
+                            public class SystemClass extends RemovedPublicClass {
+                                @Hide
+                                @Override
+                                public void method() {}
+                            }
+                        """
+                    ),
+                ),
+            expectedIssues =
+                """
+                    src/test/pkg/SystemClass.java:7: warning: Public class test.pkg.SystemClass stripped of unavailable superclass test.pkg.RemovedPublicClass [HiddenSuperclass]
+                """,
+            expectedApiSignature = "",
+            // SystemClass is part of the system API surface (not module-lib) and its
+            // overriding method is marked @Hide, overriding a method from a removed public
+            // class. Therefore, neither the class nor the method is emitted into the
+            // module-lib removed API signature file, even with add-additional-overrides=yes.
+            removedApi = "",
+        )
+    }
+}
