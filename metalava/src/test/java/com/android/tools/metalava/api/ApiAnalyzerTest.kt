42de6695c11842f6c008c8dff3ede738cfa29a7b
Julia McClellan <juliamcclellan@google.com>
2026-09-11 19:36:35 -0400

    Add test showing MismatchedDeprecated behavior with ignore comments
    This check uses documentation, so the results don't make sense when documentation is not processed.
    
    Bug: 555294492
    Test: ApiAnalyzerTest
    Change-Id: I5fae180db68166fa56387d56f020cfd5b29c8791

--- metalava/src/test/java/com/android/tools/metalava/api/ApiAnalyzerTest.kt
+++ metalava/src/test/java/com/android/tools/metalava/api/ApiAnalyzerTest.kt
@@ -244,6 +244,44 @@ class ApiAnalyzerTest : DriverTest() {
     }
 
     @Test
+    fun `Test that DeprecationMismatch is not reported when comments are ignored`() {
+        // TODO: when comments aren't read, deprecation mismatch shouldn't be reported
+        check(
+            expectedIssues =
+                """
+                src/test/pkg/CorrectDeprecation.java:4: error: Class test.pkg.CorrectDeprecation: @Deprecated annotation (present) and @deprecated doc tag (not present) do not match [DeprecationMismatch]
+                src/test/pkg/MissingDeprecatedDoc.java:3: error: Class test.pkg.MissingDeprecatedDoc: @Deprecated annotation (present) and @deprecated doc tag (not present) do not match [DeprecationMismatch]
+                """,
+            sourceFiles =
+                arrayOf(
+                    java(
+                        """
+                        package test.pkg;
+                        @Deprecated
+                        public class MissingDeprecatedDoc {}
+                        """
+                    ),
+                    java(
+                        """
+                        package test.pkg;
+                        /** @deprecated reason */
+                        public class MissingDeprecatedAnno {}
+                        """
+                    ),
+                    java(
+                        """
+                        package test.pkg;
+                        /** @deprecated reason */
+                        @Deprecated
+                        public class CorrectDeprecation {}
+                        """
+                    )
+                ),
+            extraArguments = arrayOf(ARG_SKIP_READING_COMMENTS),
+        )
+    }
+
+    @Test
     fun `Test inheriting methods from hidden class preserves deprecated status`() {
         check(
             sourceFiles =
